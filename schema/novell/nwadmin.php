2dc47db3b13ee8dd103303e413a24a8fcb9a3f3d
James Turner <james@turnersoft.co.uk>
2016-05-17 09:41:52 +0100

    added Novell NetWare Administrator schema

---
+++ schema/novell/nwadmin.php
@@ -0,0 +1,16 @@
+<?php
+/** Novell NetWare Administrator schema (partial) */
+
+class novell_nwadmin_schema extends ldap_schema
+{
+	function __construct(&$ldap_server)
+	{
+		// Structural object classes
+		$this->object_schema = array(
+			array("name"=>"Template",			"icon"=>"novell/template.png",		"is_folder"=>false,"display_name"=>gettext("Template"))
+			);
+
+		parent::__construct($ldap_server);
+	}
+}
+?>
