dddef91d74d1b2d1d808dfac54bb0bbd6c5721b1
James Turner <james@turnersoft.co.uk>
2016-05-19 09:39:57 +0100

    added NLS license container and certificate object classes

--- schema/novell/nls.php
+++ schema/novell/nls.php
@@ -9,6 +9,8 @@ class novell_nls_schema extends ldap_schema
 
 		$this->object_schema = array(
 			array("name"=>"nLSLicenseServer",		"icon"=>"novell/license-server.png",	"is_folder"=>false,"display_name"=>gettext("License Service Provider")),
+			array("name"=>"nLSProductContainer",		"icon"=>"novell/license-container.png",	"is_folder"=>true,"rdn_attrib"=>"nLSPublisher,nLSProduct,nLSVersion","required_attribs"=>"nLSRevision","display_name"=>gettext("License Product Container")),
+			array("name"=>"nLSLicenseCertificate",		"icon"=>"novell/license-cert.png",	"is_folder"=>false,"rdn_attrib"=>"nLSLicenseID","display_name"=>gettext("License Certificate")),
 			);
 
 		parent::__construct($ldap_server);
