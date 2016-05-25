900172301681b9ccd92a484e7ae16c7d486b3682
James Turner <james@turnersoft.co.uk>
2016-05-25 08:54:55 +0100

    moved cert-authority.png to shared icon area

--- schema/novell/ndspki.php
+++ schema/novell/ndspki.php
@@ -24,7 +24,7 @@ class novell_ndspki_schema extends ldap_schema
 			array("name"=>"nDSPKIKeyMaterial",		"icon"=>"novell/key-material.png",	"is_folder"=>false,"display_name"=>gettext("Key Material")),
 			array("name"=>"nDSPKISDKeyAccessPartition",	"icon"=>"novell/sd-key-access-partition.png","is_folder"=>true,"display_name"=>gettext("Security Domain Key Access Partition")),
 			array("name"=>"nDSPKISDKeyList",		"icon"=>"novell/sd-key-list.png",	"is_folder"=>true,"display_name"=>gettext("Security Domain Key List")),
-			array("name"=>"nDSPKICertificateAuthority",	"icon"=>"novell/cert-authority.png",	"is_folder"=>false,"display_name"=>gettext("Certificate Authority"))
+			array("name"=>"nDSPKICertificateAuthority",	"icon"=>"cert-authority.png",		"is_folder"=>false,"display_name"=>gettext("Certificate Authority"))
 			);
 
 		parent::__construct($ldap_server);
