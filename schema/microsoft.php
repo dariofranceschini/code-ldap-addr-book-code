4ee1af6d38f70057839bc8a52cc5d12bf832c355
James Turner <james@turnersoft.co.uk>
2016-06-05 12:32:35 +0100

    added various further Microsoft schema attributes

--- schema/microsoft.php
+++ schema/microsoft.php
@@ -6,11 +6,17 @@ class microsoft_schema extends ldap_schema
 	function __construct(&$ldap_server)
 	{
 		$this->attribute_schema = array(
+			array("name"=>"adminContextMenu",		"data_type"=>"text_list",	"display_name"=>gettext("Admin Context Menu Entries")),
+			array("name"=>"adminPropertyPages",		"data_type"=>"text_list",	"display_name"=>gettext("Admin Property Pages")),
+			array("name"=>"attributeDisplayNames",		"data_type"=>"text_list",	"display_name"=>gettext("Attribute Display Names")),
 			array("name"=>"company",			"data_type"=>"text",		"display_name"=>gettext("Company")),
+			array("name"=>"contextMenu",			"data_type"=>"text_list",	"display_name"=>gettext("Context Menu Entries")),
+			array("name"=>"createWizardExt",		"data_type"=>"text_list",	"display_name"=>gettext("Object Creation Wizard Extensions")),
 			array("name"=>"department",			"data_type"=>"text",		"display_name"=>gettext("Department")),
 			array("name"=>"displayName",			"data_type"=>"text",		"display_name"=>gettext("Display/Preferred Name")),
 			array("name"=>"groupType",			"data_type"=>"ad_group_type",	"display_name"=>gettext("Group Type/Scope")),
 			array("name"=>"info",				"data_type"=>"text_area",	"display_name"=>gettext("General Information")),
+			array("name"=>"managedBy",			"data_type"=>"dn",		"display_name"=>gettext("Managed By")),
 			array("name"=>"memberOf",			"data_type"=>"dn_list",		"display_name"=>gettext("Member Of")),
 			array("name"=>"printColor",			"data_type"=>"yes_no",		"display_name"=>gettext("Color Printing Supported")),
 			array("name"=>"printDuplexSupported",		"data_type"=>"yes_no",		"display_name"=>gettext("Double-Sided Printing Supported")),
@@ -18,11 +24,26 @@ class microsoft_schema extends ldap_schema
 			array("name"=>"printMediaSupported",		"data_type"=>"text_list",	"display_name"=>gettext("Paper Supported")),
 			array("name"=>"printStaplingSupported",		"data_type"=>"yes_no",		"display_name"=>gettext("Stapling Supported")),
 			array("name"=>"sAMAccountName",			"data_type"=>"text",		"display_name"=>gettext("Pre-Windows 2000 Account Name")),
+
+			// Data type of "serverName" is changed to "dn" when used with the Active Directory
+			// Root DSE object (hardcoded workaround). The data type "text" (as defined below) is
+			// used for the "serverName" attribute of "printQueue" objects.
+			array("name"=>"serverName",			"data_type"=>"text",		"display_name"=>gettext("Server Name")),
+
+			array("name"=>"servicePrincipalName",		"data_type"=>"text_list",	"display_name"=>gettext("Service Principal Names")),
+			array("name"=>"shellPropertyPages",		"data_type"=>"text_list",	"display_name"=>gettext("Shell Property Pages")),
+			// Non-standard implementation of streetAddress as a separate attribute, rather than an alias of street:
 			array("name"=>"streetAddress",			"data_type"=>"text_area",	"display_name"=>gettext("Street Address")),
+			array("name"=>"treatAsLeaf",			"data_type"=>"yes_no",		"display_name"=>gettext("Treat as Leaf Object")),
 			array("name"=>"url",				"data_type"=>"text",		"display_name"=>gettext("URL (e.g. web page)")),
 			array("name"=>"whenCreated",			"data_type"=>"date_time",	"display_name"=>gettext("Creation Date")),
 			array("name"=>"whenChanged",			"data_type"=>"date_time",	"display_name"=>gettext("Last Modification Date")),
-			array("name"=>"wWWHomePage",			"data_type"=>"text",		"display_name"=>gettext("WWW Home Page"))
+			array("name"=>"wWWHomePage",			"data_type"=>"text",		"display_name"=>gettext("WWW Home Page")),
+
+			// Added for Windows Server 2003
+			array("name"=>"adminMultiSelectPropertyPages",	"data_type"=>"text_list",	"display_name"=>gettext("Admin Multi Select Property Pages")),
+			array("name"=>"extraColumns",			"data_type"=>"text_list",	"display_name"=>gettext("Extra Columns")),
+			array("name"=>"msDS-AllowedToDelegateTo",	"data_type"=>"text_list",	"display_name"=>gettext("Allowed to Delegate To"))
 			);
 
 		// Structural object classes
