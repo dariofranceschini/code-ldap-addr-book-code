43c82e0e465551e1c5c661318a0453e8bc9d7d0a
James Turner <james@turnersoft.co.uk>
2016-06-26 10:33:14 +0100

    whitespace fix in Microsoft schema

--- schema/microsoft.php
+++ schema/microsoft.php
@@ -49,41 +49,41 @@ class microsoft_schema extends ldap_schema
 		// Structural object classes
 		$this->object_schema = array(
 			array("name"=>"rpcContainer",			"icon"=>"microsoft/rpc_services24.png",	"is_folder"=>true,"display_name"=>gettext("RPC Services")),
-			array("name"=>"container",			"icon"=>"folder.png",	"is_folder"=>true,"display_name"=>gettext("Container"),"can_create"=>true),
-			array("name"=>"builtinDomain",			"icon"=>"folder.png",	"is_folder"=>true),
-			array("name"=>"lostAndFound",			"icon"=>"folder.png",	"is_folder"=>true),
-			array("name"=>"msDS-QuotaContainer",		"icon"=>"folder.png",	"is_folder"=>true),
+			array("name"=>"container",			"icon"=>"folder.png",			"is_folder"=>true,"display_name"=>gettext("Container"),"can_create"=>true),
+			array("name"=>"builtinDomain",			"icon"=>"folder.png",			"is_folder"=>true),
+			array("name"=>"lostAndFound",			"icon"=>"folder.png",			"is_folder"=>true),
+			array("name"=>"msDS-QuotaContainer",		"icon"=>"folder.png",			"is_folder"=>true),
 			array("name"=>"domainDNS",			"icon"=>"microsoft/domain24.png",	"is_folder"=>true,"display_name"=>gettext("Domain")),
-			array("name"=>"group",				"icon"=>"group24.png",	"is_folder"=>false,"display_name"=>gettext("Group"),"can_create"=>true),
-			array("name"=>"contact",			"icon"=>"contact24.png","is_folder"=>false,"display_name"=>gettext("Contact"),"can_create"=>true),
-			array("name"=>"computer",			"icon"=>"microsoft/computer24.png","is_folder"=>false,"display_name"=>gettext("Computer"),"can_create"=>true),
-			array("name"=>"foreignSecurityPrincipal",	"icon"=>"user-alias24.png","is_folder"=>false,"display_name"=>gettext("Foreign Security Principal")),
-			array("name"=>"printQueue",			"icon"=>"microsoft/printer24.png","is_folder"=>false,"display_name"=>gettext("Printer")),
-			array("name"=>"volume",				"icon"=>"microsoft/fileshare24.png","is_folder"=>false,"display_name"=>gettext("Shared Folder")),
-			array("name"=>"user",				"icon"=>"user24.png",	"is_folder"=>false,"display_name"=>gettext("User"),"can_create"=>true),
+			array("name"=>"group",				"icon"=>"group24.png",			"is_folder"=>false,"display_name"=>gettext("Group"),"can_create"=>true),
+			array("name"=>"contact",			"icon"=>"contact24.png",		"is_folder"=>false,"display_name"=>gettext("Contact"),"can_create"=>true),
+			array("name"=>"computer",			"icon"=>"microsoft/computer24.png",	"is_folder"=>false,"display_name"=>gettext("Computer"),"can_create"=>true),
+			array("name"=>"foreignSecurityPrincipal",	"icon"=>"user-alias24.png",		"is_folder"=>false,"display_name"=>gettext("Foreign Security Principal")),
+			array("name"=>"printQueue",			"icon"=>"microsoft/printer24.png",	"is_folder"=>false,"display_name"=>gettext("Printer")),
+			array("name"=>"volume",				"icon"=>"microsoft/fileshare24.png",	"is_folder"=>false,"display_name"=>gettext("Shared Folder")),
+			array("name"=>"user",				"icon"=>"user24.png",			"is_folder"=>false,"display_name"=>gettext("User"),"can_create"=>true),
 
 			// Proprietary implementation of InetOrgPerson:
 			//	- Subclass of proprietary "user" (so listed after it in schema definition)
 			//	- Attribute "sn" is not mandatory
-			array("name"=>"inetOrgPerson",			"icon"=>"user24.png",	"is_folder"=>false,"display_name"=>gettext("InetOrgPerson"),"can_create"=>true),
+			array("name"=>"inetOrgPerson",			"icon"=>"user24.png",			"is_folder"=>false,"display_name"=>gettext("InetOrgPerson"),"can_create"=>true),
 
 			// Specialist object classes used in Configuration Partition to be shown as folders
-			array("name"=>"crossRefContainer",		"icon"=>"folder.png","is_folder"=>true),
-			array("name"=>"sitesContainer",			"icon"=>"folder.png","is_folder"=>true),
-			array("name"=>"interSiteTransport",		"icon"=>"folder.png","is_folder"=>true),
-			array("name"=>"interSiteTransportContainer",	"icon"=>"folder.png","is_folder"=>true),
-			array("name"=>"subnetContainer",		"icon"=>"folder.png","is_folder"=>true),
-			array("name"=>"site",				"icon"=>"folder.png","is_folder"=>true),
-			array("name"=>"serversContainer",		"icon"=>"folder.png","is_folder"=>true),
-			array("name"=>"server",				"icon"=>"folder.png","is_folder"=>true),
-			array("name"=>"nTDSDSA",			"icon"=>"folder.png","is_folder"=>true),
-			array("name"=>"nTDSService",			"icon"=>"folder.png","is_folder"=>true),
+			array("name"=>"crossRefContainer",		"icon"=>"folder.png",			"is_folder"=>true),
+			array("name"=>"sitesContainer",			"icon"=>"folder.png",			"is_folder"=>true),
+			array("name"=>"interSiteTransport",		"icon"=>"folder.png",			"is_folder"=>true),
+			array("name"=>"interSiteTransportContainer",	"icon"=>"folder.png",			"is_folder"=>true),
+			array("name"=>"subnetContainer",		"icon"=>"folder.png",			"is_folder"=>true),
+			array("name"=>"site",				"icon"=>"folder.png",			"is_folder"=>true),
+			array("name"=>"serversContainer",		"icon"=>"folder.png",			"is_folder"=>true),
+			array("name"=>"server",				"icon"=>"folder.png",			"is_folder"=>true),
+			array("name"=>"nTDSDSA",			"icon"=>"folder.png",			"is_folder"=>true),
+			array("name"=>"nTDSService",			"icon"=>"folder.png",			"is_folder"=>true),
 
 			// Specialist object classes used in "System" container
 			array("name"=>"domainPolicy",			"icon"=>"microsoft/domain_policy24.png","is_folder"=>true,"display_name"=>gettext("Domain Policy")),
 			array("name"=>"nTFRSSettings",			"icon"=>"microsoft/frs_settings24.png",	"is_folder"=>true,"display_name"=>gettext("FRS Settings")),
-			array("name"=>"fileLinkTracking",		"icon"=>"folder.png","is_folder"=>true),
-			array("name"=>"dfsConfiguration",		"icon"=>"folder.png","is_folder"=>true),
+			array("name"=>"fileLinkTracking",		"icon"=>"folder.png",			"is_folder"=>true),
+			array("name"=>"dfsConfiguration",		"icon"=>"folder.png",			"is_folder"=>true),
 			);
 
 		// component schema (derived)
