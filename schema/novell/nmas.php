60ebec093a9dac3438a67245a1e072d8b6e756cf
James Turner <james@turnersoft.co.uk>
2016-07-28 09:17:28 +0100

    added display layout for eDirectory NMAS/SAS Login Policy objects

--- schema/novell/nmas.php
+++ schema/novell/nmas.php
@@ -14,6 +14,28 @@ class novell_nmas_schema extends ldap_schema
 			array("name"=>"sASLoginPolicy",			"icon"=>"novell/login-policy.png",	"is_folder"=>false,"display_name"=>gettext("Login Policy")),
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("sASLoginPolicy",array(
+			array("section_name"=>gettext("Login Policy"),"new_row"=>true,
+				"attributes"=>array(
+					array("sasAuditConfiguration",		gettext("Enable Auditing"),			"generic24.png"),
+
+					array("sasLoginSequence",		gettext("NMAS Login Sequences"),		"generic24.png"),
+					array("sasDefaultLoginSequence",	gettext("Default Login Sequence"),		"generic24.png"),
+
+					// Binary attributes - no handlers to display yet
+
+				//	array("sasPolicyCredentials",		gettext("Policy Credentials"),			"generic24.png"),
+				//	array("sasNMASProductOptions",		gettext("NMAS Product Options"),		"generic24.png"),
+				//	array("masvAuthorizedRange",		gettext("MASV Authorized Range"),		"generic24.png"),
+				//	array("masvDefaultRange",		gettext("MASV Default Range"),			"generic24.png"),
+
+					array("sasLoginPolicyUpdate",		gettext("Policy Update"),			"generic24.png"),
+					array("sasPolicyObjectVersion",		gettext("Policy Object Version"),		"generic24.png")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
