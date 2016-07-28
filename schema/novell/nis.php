d068da9d37f357ba316b88833b1e8685d9eb23da
James Turner <james@turnersoft.co.uk>
2016-07-28 09:41:56 +0100

    added display layout for eDirectory NIS Server objects

--- schema/novell/nis.php
+++ schema/novell/nis.php
@@ -11,6 +11,16 @@ class novell_nis_schema extends ldap_schema
 			array("name"=>"nisServer",		"icon"=>"novell/nis-server.png",		"is_folder"=>false),
 			);
 
+		// Display layouts
+
+		$ldap_server->add_display_layout("nisServer",array(
+			array("section_name"=>gettext("NIS Server"),
+				"attributes"=>array(
+					array("ipHostNumber",		gettext("IP Address"),			"generic24.png")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
