c75887d29b6b725df2deb1436a425f21f07a70c3
James Turner <james@turnersoft.co.uk>
2016-07-28 08:58:53 +0100

    added display layout for eDirectory WAN Traffic Manager LAN Area objects

--- schema/novell/wanman.php
+++ schema/novell/wanman.php
@@ -14,6 +14,16 @@ class novell_wanman_schema extends ldap_schema
 			array("name"=>"wANMANLANArea",		"icon"=>"novell/wanman-lan-area.png",	"is_folder"=>false,"display_name"=>gettext("WAN Traffic Manager LAN Area")),
 			);
 
+		// Display layouts
+
+		$ldap_server->add_display_layout("wANMANLANArea",array(
+			array("section_name"=>gettext("WAN Traffic Manager Policies"),
+				"attributes"=>array(
+					array("wANMANWANPolicy")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
