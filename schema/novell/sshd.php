0d865a6961801aa0c3c489d5bfce2e2aa6931d3b
James Turner <james@turnersoft.co.uk>
2016-07-28 09:32:39 +0100

    added display layout for eDirectory SSH Server objects

--- schema/novell/sshd.php
+++ schema/novell/sshd.php
@@ -15,7 +15,32 @@ class novell_sshd_schema extends ldap_schema
 			array("name"=>"sshadmnServer",		"icon"=>"novell/sshd-server.png",	"is_folder"=>true),
 			);
 
+		// Display layouts
+
+		$ldap_server->add_display_layout("sshadmnServer",array(
+			array("section_name"=>gettext("SSH Server"),
+				"attributes"=>array(
+					array("cn",				gettext("Object Name"),		"generic24.png"),
+					array("sshadmn-ServerName",		gettext("Server Name"),		"generic24.png"),
+					array("sshadmn-ServerStatus",		gettext("Server Status"),	"generic24.png"),
+					array("sshadmn-ServerStartup",		gettext("Server Startup"),	"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Configuration Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("sshadmn-ConfigurationInfo")
+					)
+				),
+			array("section_name"=>gettext("Configuration Info"),"new_row"=>true,
+				"attributes"=>array(
+					array("sshadmn-ServerConfLastModified",	gettext("Last Modified"),	"generic24.png"),
+					array("sshadmn-TypeName",		gettext("Type Name"),		"generic24.png")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
+
 ?>
