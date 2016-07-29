bdff2511ff3622140cf4fc8061064d7f18d9638e
James Turner <james@turnersoft.co.uk>
2016-07-29 09:37:59 +0100

    added display layout for eDirectory NSS Storage Pool objects

--- schema/novell/nssfs.php
+++ schema/novell/nssfs.php
@@ -10,6 +10,17 @@ class novell_nssfs_schema extends ldap_schema
 			array("name"=>"nssfsPool",			"icon"=>"novell/nssfs-pool.png",	"is_folder"=>false)
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("nssfsPool",array(
+			array("section_name"=>gettext("Storage Pool"),
+				"attributes"=>array(
+					array("hostServer",			gettext("Host Server"),					"generic24.png"),
+					array("hostResourceName",		gettext("Host Resource Name"),				"generic24.png"),
+					array("nssfsPoolID",			gettext("Pool ID"),					"generic24.png")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
