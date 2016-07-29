d72c761aee8dd0b906de54822d1efb77d05be0fd
James Turner <james@turnersoft.co.uk>
2016-07-29 09:19:34 +0100

    added display layouts for eDirectory SLP schema objects

--- schema/novell/slp.php
+++ schema/novell/slp.php
@@ -12,6 +12,26 @@ class novell_slp_schema extends ldap_schema
 			array("name"=>"SLPService",		"icon"=>"novell/slp-service.png",		"is_folder"=>false,"display_name"=>gettext("SLP Service"))
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("SLPScopeUnit",array(
+			array("section_name"=>gettext("SLP Scope Unit"),
+				"attributes"=>array(
+					array("su",			gettext("Name"),			"generic24.png"),
+					)
+				),
+			));
+
+		$ldap_server->add_display_layout("SLPService",array(
+			array("section_name"=>gettext("SLP Service"),
+				"attributes"=>array(
+					array("SLPType",		gettext("Type"),			"generic24.png"),
+					array("SLPLifetime",		gettext("Lifetime"),			"generic24.png"),
+					array("SLPLanguage",		gettext("Language"),			"generic24.png"),
+					array("SLPURL",			gettext("URL"),				"generic24.png")
+					)
+				),
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
