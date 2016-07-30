199b89b6418798c3fb24ddc1bd1481220cacfca1
James Turner <james@turnersoft.co.uk>
2016-07-30 12:28:09 +0100

    added display layouts for ISC DHCP Server schema objects

--- schema/dhcp.php
+++ schema/dhcp.php
@@ -28,6 +28,155 @@ class dhcp_schema extends ldap_schema
 			array("name"=>"dhcpTSigKey",			"icon"=>"dhcp/dhcp-dns-key.png",	"is_folder"=>false,"display_name"=>gettext("DHCP TSIG Key"))
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("dhcpClass",array(
+			array("section_name"=>gettext("DHCP Class Configuration"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpStatements")
+					)
+				),
+			array("section_name"=>gettext("DHCP Options"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpOption")
+					)
+				),
+			array("section_name"=>gettext("Comments"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpComments")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("dhcpDnsZone",array(
+			array("section_name"=>gettext("DHCP Dynamic DNS Update Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpDNSZoneServer",		gettext("DNS Server"),			"generic24.png"),
+					array("dhcpKeyDN",			gettext("TSIG Key"),			"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Comments"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpComments")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("dhcpFailOverPeer",array(
+			array("section_name"=>gettext("DHCP Failover Peer"),"colspan"=>2,
+				"attributes"=>array(
+					array("dhcpFailOverLoadBalanceTime",	gettext("Load Balance Time (s)"),	"generic24.png"),
+					array("dhcpFailOverSplit",		gettext("Split"),			"generic24.png"),
+					array("dhcpMaxClientLeadTime",		gettext("Maximum Client Lead Time (s)"),"generic24.png"),
+					array("dhcpFailOverResponseDelay",	gettext("Response Delay (s)"),		"generic24.png"),
+					array("dhcpFailOverUnackedUpdates",	gettext("Unacknowledged Updates"),	"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Primary"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpFailOverPrimaryServer",	gettext("Server"),			"generic24.png"),
+					array("dhcpFailOverPrimaryPort",	gettext("Port"),			"generic24.png"),
+					)
+				),
+			array("section_name"=>gettext("Secondary"),"width"=>"50%",
+				"attributes"=>array(
+					array("dhcpFailOverSecondaryServer",	gettext("Server"),			"generic24.png"),
+					array("dhcpFailOverSecondaryPort",	gettext("Port"),			"generic24.png"),
+					)
+				),
+			array("section_name"=>gettext("Comments"),"new_row"=>true,"colspan"=>2,
+				"attributes"=>array(
+					array("dhcpComments")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("dhcpHost",array(
+			array("section_name"=>gettext("Host Information"),
+				"attributes"=>array(
+					array("dhcpHWAddress",			gettext("Hardware Address"),		"generic24.png"),
+					)
+				),
+			array("section_name"=>gettext("DHCP Server Configuration"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpStatements")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("dhcpLocator",array(
+			array("section_name"=>gettext("DHCP Object Locations"),
+				"attributes"=>array(
+					array("dhcpServerDN",			gettext("Server Objects"),		"generic24.png"),
+					array("dhcpServiceDN",			gettext("Service Objects"),		"generic24.png")
+					)
+				),
+			));
+
+		$ldap_server->add_display_layout("dhcpServer",array(
+			array("section_name"=>("Associated Objects"),
+				"attributes"=>array(
+					array("dhcpLocatorDN",			gettext("DHCP Locator Object"),		"generic24.png"),
+					array("dhcpServiceDN",			gettext("DHCP Service Object"),		"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("DHCP Server Configuration"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpStatements")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("dhcpSharedNetwork",array(
+			array("section_name"=>gettext("Shared Network Configuration"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpStatements")
+					)
+				),
+			array("section_name"=>gettext("DHCP Options"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpOption")
+					)
+				),
+			array("section_name"=>gettext("Comments"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpComments")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("dhcpSubnet",array(
+			array("section_name"=>gettext("Subnet Information"),
+				"attributes"=>array(
+					array("dhcpNetmask",			gettext("Netmask"),			"generic24.png"),
+					)
+				),
+			array("section_name"=>gettext("DHCP Server Configuration"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpStatements")
+					)
+				),
+			array("section_name"=>gettext("Comments"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpComments")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("dhcpTSigKey",array(
+			array("section_name"=>gettext("DHCP Transaction Signature (TSIG) Key"),"new_row"=>true,
+				"attributes"=>array(
+
+					array("dhcpKeyAlgorithm",		gettext("Algorithm"),			"generic24.png"),
+					array("dhcpKeySecret",			gettext("Secret"),			"generic24.png"),
+					)
+				),
+			array("section_name"=>gettext("Comments"),"new_row"=>true,
+				"attributes"=>array(
+					array("dhcpComments")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
