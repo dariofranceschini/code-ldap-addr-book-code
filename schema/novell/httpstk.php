5f96f073366e2e329319a03b66160f5f31bdef77
James Turner <james@turnersoft.co.uk>
2016-07-30 10:17:02 +0100

    added display layout for eDirectory iMonitor HTTP Server objects

--- schema/novell/httpstk.php
+++ schema/novell/httpstk.php
@@ -16,6 +16,30 @@ class novell_httpstk_schema extends ldap_schema
 			array("name"=>"httpServer",			"icon"=>"novell/http-server.png",	"is_folder"=>false),
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("httpServer",array(
+			array("section_name"=>gettext("HTTP Server (iMonitor)"),"new_row"=>true,
+				"attributes"=>array(
+					array("httpHostServerDN",		gettext("Host Server"),				"generic24.png"),
+					array("httpDefaultClearPort",		gettext("Default Port - HTTP"),			"generic24.png"),
+					array("httpDefaultTLSPort",		gettext("Default Port - HTTPS"),		"generic24.png"),
+					array("httpAuthRequiresTLS",		gettext("TLS Required for Authentication"),	"generic24.png"),
+					array("httpKeyMaterialObject",		gettext("Certificate"),				"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Server Configuration"),"new_row"=>true,
+				"attributes"=>array(
+					array("httpBindRestrictions",		gettext("Bind Restrictions"),			"generic24.png"),
+					array("httpTraceLevel",			gettext("Trace Level"),				"generic24.png"),
+					array("httpSessionTimeout",		gettext("Session Timeout (s)"),			"generic24.png"),
+					array("httpKeepAliveRequestTimeout",	gettext("Keep-Alive Request Timeout (s)"),	"generic24.png"),
+					array("httpRequestTimeout",		gettext("Request Timeout (s)"),			"generic24.png"),
+					array("httpIOBufferSize",		gettext("I/O Buffer Size"),			"generic24.png"),
+					array("httpThreadsPerCPU",		gettext("Threads per CPU"),			"generic24.png")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
