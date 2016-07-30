e640de35baa2dcf721de1fa72547bce6a1c83cc8
James Turner <james@turnersoft.co.uk>
2016-07-30 11:41:57 +0100

    added display layouts for eDirectory UAM schema objects

--- schema/novell/uam.php
+++ schema/novell/uam.php
@@ -17,6 +17,41 @@ class novell_uam_schema extends ldap_schema
 			array("name"=>"uamPosixConfig",			"icon"=>"novell/unix-config.png",	"is_folder"=>false),
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("uamPosixConfig",array(
+			array("section_name"=>gettext("Workstation Contexts for Unix Account Management"),"colspan"=>2,
+				"attributes"=>array(
+					array("uamPosixWorkstationContexts"),
+					)
+				),
+			array("section_name"=>gettext("User ID Mapping"),"new_row"=>true,
+				"attributes"=>array(
+					array("uamPosixUidNumberStart",		gettext("Starting UID"),			"generic24.png"),
+					array("uamPosixUidNumberEnd",		gettext("End UID"),				"generic24.png"),
+					array("uamPosixUidNumberLastAssigned",	gettext("Last Assigned UID"),			"generic24.png"),
+					array("uamPosixUidNumberReuse",		gettext("Reuse UID Numbers"),			"generic24.png"),
+			//		array("uamPosixUidNumberDeletedMap",	gettext("Deleted UID Map"),			"generic24.png"),
+					)
+				),
+			array("section_name"=>gettext("Group ID Mapping"),"width"=>"50%",
+				"attributes"=>array(
+					array("uamPosixGidNumberStart",		gettext("Starting GID"),			"generic24.png"),
+					array("uamPosixGidNumberEnd",		gettext("End GID"),				"generic24.png"),
+					array("uamPosixGidNumberLastAssigned",	gettext("Last Assigned GID"),			"generic24.png"),
+					array("uamPosixGidNumberReuse",		gettext("Reuse GID Numbers"),			"generic24.png"),
+			//		array("uamPosixGidNumberDeletedMap",	gettext("Deleted GID Map"),			"generic24.png"),
+					)
+				),
+			));
+
+		$ldap_server->add_display_layout("uamPosixWorkstation",array(
+			array("section_name"=>gettext("Groups with access to LUM-enabled Services"),"new_row"=>true,
+				"attributes"=>array(
+					array("groupMembership")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
