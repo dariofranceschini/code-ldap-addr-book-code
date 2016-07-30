ba071920467d0089742b9d902ae65cdb54a6d0e7
James Turner <james@turnersoft.co.uk>
2016-07-30 09:50:55 +0100

    added display layout for eDirectory SNMP Group objects

--- schema/novell/snmp.php
+++ schema/novell/snmp.php
@@ -17,6 +17,23 @@ class novell_snmp_schema extends ldap_schema
 			array("name"=>"snmpGroup",				"icon"=>"novell/snmp-group.png",	"is_folder"=>false),
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("snmpGroup",array(
+			array("section_name"=>gettext("SNMP Group"),"new_row"=>true,
+				"attributes"=>array(
+					array("snmpServerList",			gettext("SNMP Servers"),			"generic24.png"),
+					array("snmpTrapInterval",		gettext("Default SNMP Time Interval (s)"),	"generic24.png"),
+					array("snmpTrapDisable",		gettext("Disable all SNMP Traps"),		"generic24.png"),
+
+				/*	TODO: decode SNMP trap configuration data
+					array("snmpTrapDescription",		gettext("Trap Descriptions"),			"generic24.png"),
+					array("snmpTrapConfig",			gettext("Trap Configuration"),			"generic24.png"), */
+
+					array("version",			gettext("Version"),				"generic24.png"),
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
