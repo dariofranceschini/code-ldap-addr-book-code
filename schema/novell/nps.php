378962ad731dac01591cf03af53ff292314563c0
James Turner <james@turnersoft.co.uk>
2016-07-30 11:03:18 +0100

    added display layouts for eDirectory NPS schema objects

--- schema/novell/nps.php
+++ schema/novell/nps.php
@@ -31,6 +31,178 @@ class novell_nps_schema extends ldap_schema
 			array("name"=>"bhTheme",	"icon"=>"logo24.png",		"is_folder"=>false)
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("bhGadget",array(
+			array("section_name"=>gettext("Portal Gadget"),
+				"attributes"=>array(
+					array("cn",			gettext("Name"),			"generic24.png"),
+					array("bhLocation",		gettext("Location"),			"generic24.png"),
+					array("bhVersion",		gettext("Version"),			"generic24.png"),
+					array("bhObjectGUID",		gettext("Object GUID"),			"generic24.png"),
+					array("bhInstanceClassName",	gettext("Instance Class Name"),		"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Configuration"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhConfig")
+					)
+				),
+			array("section_name"=>gettext("Other Objects"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhObjectList")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("bhModule",array(
+			array("section_name"=>gettext("Portal Gadget"),
+				"attributes"=>array(
+					array("cn",			gettext("Name"),			"generic24.png"),
+					array("bhLocation",		gettext("Location"),			"generic24.png"),
+					array("bhVersion",		gettext("Version"),			"generic24.png"),
+					array("bhObjectGUID",		gettext("Object GUID"),			"generic24.png"),
+					array("bhPortalDN",		gettext("Portal DN"),			"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Configuration"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhConfig")
+					)
+				),
+			array("section_name"=>gettext("Gadgets"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhGadgetList")
+					)
+				),
+			array("section_name"=>gettext("Page Sets"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhPageSetList")
+					)
+				),
+			array("section_name"=>gettext("Themes"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhThemeList")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("bhPortal",array(
+			array("section_name"=>gettext("Portal"),
+				"attributes"=>array(
+					array("cn",			gettext("Name"),			"generic24.png"),
+					array("bhLocation",		gettext("Location"),			"generic24.png"),
+					array("bhVersion",		gettext("Version"),			"generic24.png"),
+					array("bhObjectGUID",		gettext("Object GUID"),			"generic24.png"),
+					array("bhCommunityContainer",	gettext("Community Container"),		"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Configuration"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhConfig")
+					)
+				),
+			array("section_name"=>gettext("Modules"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhModuleList")
+					)
+				),
+			array("section_name"=>gettext("Pages"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhPageList")
+					)
+				),
+			array("section_name"=>gettext("Page Sets"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhPageSetList")
+					)
+				),
+			array("section_name"=>gettext("Themes"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhThemeList")
+					)
+				),
+			array("section_name"=>gettext("Other Objects"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhObjectList")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("bhPage",array(
+			array("section_name"=>gettext("Portal Page"),
+				"attributes"=>array(
+					array("cn",			gettext("Name"),			"generic24.png"),
+					array("bhObjectGUID",		gettext("Object GUID"),			"generic24.png"),
+					array("bhPortalDN",		gettext("Portal DN"),			"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Configuration"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhConfig")
+					)
+				),
+			array("section_name"=>gettext("Parent Modules"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhParentModuleList")
+					)
+				),
+			array("section_name"=>gettext("Other Objects"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhObjectList")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("bhTheme",array(
+			array("section_name"=>gettext("Portal Theme"),
+				"attributes"=>array(
+					array("cn",			gettext("Name"),			"generic24.png"),
+					array("bhObjectGUID",		gettext("Object GUID"),			"generic24.png"),
+					array("bhPortalDN",		gettext("Portal DN"),			"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Configuration"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhConfig")
+					)
+				),
+			array("section_name"=>gettext("Parent Modules"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhParentModuleList")
+					)
+				),
+			array("section_name"=>gettext("Other Objects"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhObjectList")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("bhPageSet",array(
+			array("section_name"=>gettext("Portal Page Set"),
+				"attributes"=>array(
+					array("cn",			gettext("Name"),			"generic24.png"),
+					array("bhObjectGUID",		gettext("Object GUID"),			"generic24.png"),
+					array("bhPortalDN",		gettext("Portal DN"),			"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Configuration"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhConfig")
+					)
+				),
+			array("section_name"=>gettext("Parent Modules"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhParentModuleList")
+					)
+				),
+			array("section_name"=>gettext("GUIDs"),"new_row"=>true,
+				"attributes"=>array(
+					array("bhGUIDList")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
