165bf95db567c669591c2ca9b9ddd72debf76515
James Turner <james@turnersoft.co.uk>
2016-08-12 12:31:55 +0100

    fixed incorrect snmpGroupDN attribute data type

--- schema/novell/snmp.php
+++ schema/novell/snmp.php
@@ -8,7 +8,7 @@ class novell_snmp_schema extends ldap_schema
 		$this->attribute_schema = array(
 			array("name"=>"snmpServerList",		"data_type"=>"dn_list",		"display_name"=>gettext("SNMP Server List")),
 			array("name"=>"snmpTrapDisable",	"data_type"=>"yes_no",		"display_name"=>gettext("SNMP Traps Disabled")),
-			array("name"=>"snmpGroupDN",		"data_type"=>"dn_list",		"display_name"=>gettext("SNMP Group DN")),
+			array("name"=>"snmpGroupDN",		"data_type"=>"dn",		"display_name"=>gettext("SNMP Group DN")),
 			);
 
 		// Structural object classes
