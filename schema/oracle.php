398126ca085cb8920cf88de31f00b445d9723c94
James Turner <james@turnersoft.co.uk>
2016-08-21 09:35:54 +0100

    fixed Oracle component schema inheritence/import ordering

--- schema/oracle.php
+++ schema/oracle.php
@@ -6,10 +6,10 @@ class oracle_schema extends ldap_schema
 	function __construct(&$ldap_server)
 	{
 		// component schema
-		$ldap_server->add_schema("oracle/alias");
-		$ldap_server->add_schema("oracle/oidbase");
-		$ldap_server->add_schema("oracle/oidnet");
 		$ldap_server->add_schema("oracle/oidrdbms");
+		$ldap_server->add_schema("oracle/alias");
+		$ldap_server->add_schema("oracle/oidnet");
+		$ldap_server->add_schema("oracle/oidbase");
 	}
 }
 ?>
