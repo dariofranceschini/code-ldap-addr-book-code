561439546205a9aa92e65268d0b8acf0c13ecbbf
James Turner <james@turnersoft.co.uk>
2016-10-30 17:37:55 +0000

    updated Novell NIS schema support to recognise previously undefined object classes

--- schema/novell/nis.php
+++ schema/novell/nis.php
@@ -1,5 +1,10 @@
 <?php
-/** Novell Network Information Service (NIS) schema (partial) */
+/** Novell Network Information Service (NIS) schema (object classes only)
+
+    The abstract object classes "posixAccount" and "shadowAccount" in RFC2307
+    are defined in Novell's User Account Management (UAM) schema rather than
+    the NIS schema.
+*/
 
 class novell_nis_schema extends ldap_schema
 {
@@ -8,7 +13,24 @@ class novell_nis_schema extends ldap_schema
 		// Structural object classes
 
 		$this->object_schema = array(
-			array("name"=>"nisServer",		"icon"=>"novell/nis-server.png",		"is_folder"=>false),
+			// standard object classes defined in RFC2307
+			array("name"=>"ipNetwork",		"icon"=>"ip-network.png",		"is_folder"=>false,"display_name"=>gettext("IP Network")),
+			array("name"=>"ipProtocol",		"icon"=>"ip-protocol.png",		"is_folder"=>false,"display_name"=>gettext("IP Protocol")),
+			array("name"=>"ipService",		"icon"=>"ip-service.png",		"is_folder"=>false,"display_name"=>gettext("IP Service")),
+			array("name"=>"nisMap",			"icon"=>"nis-map.png",			"is_folder"=>true,"display_name"=>gettext("NIS Map")),
+			array("name"=>"nisNetgroup",		"icon"=>"nis-netgroup.png",		"is_folder"=>false,"display_name"=>gettext("Netgroup")),
+			array("name"=>"nisObject",		"icon"=>"nis-object.png",		"is_folder"=>false,"display_name"=>gettext("NIS Map Entry")),
+			array("name"=>"oncRpc",			"icon"=>"onc-rpc.png",			"is_folder"=>false,"display_name"=>gettext("ONC RPC Binding")),
+			// "posixGroup" (from RFC2307) is defined in UAM schema instead
+
+			// Abstract classes in RFC2307 which Novell implements as structural classes instead
+			array("name"=>"bootableDevice",		"icon"=>"novell/bootable-device.png",	"is_folder"=>false,"parent_class"=>"device"),
+			array("name"=>"ieee802Device",		"icon"=>"novell/ieee802-device.png",	"is_folder"=>false,"parent_class"=>"device"),
+			array("name"=>"ipHost",			"icon"=>"novell/ip-host.png",		"is_folder"=>false,"parent_class"=>"device"),
+
+			// Novell proprietary object classes
+			array("name"=>"nisDomain",		"icon"=>"novell/nis-domain.png",	"is_folder"=>true),
+			array("name"=>"nisServer",		"icon"=>"novell/nis-server.png",	"is_folder"=>false)
 			);
 
 		// Display layouts
