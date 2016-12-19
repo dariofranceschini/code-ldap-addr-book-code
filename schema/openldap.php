40343c1f9ec570b6ade1c80ddf147ca8f854c5e3
James Turner <james@turnersoft.co.uk>
2016-12-19 12:41:03 +0000

    add OpenLDAP Project's directory schema parent_class details

--- schema/openldap.php
+++ schema/openldap.php
@@ -7,9 +7,9 @@ class openldap_schema extends ldap_schema
 	{
                 // Structural object classes
                 $this->object_schema = array(
-                        array("name"=>"OpenLDAPorg",				"icon"=>"org.png",		"is_folder"=>true,"display_name"=>gettext("OpenLDAP Organization")),
-                        array("name"=>"OpenLDAPou",				"icon"=>"folder.png",		"is_folder"=>true,"display_name"=>gettext("OpenLDAP Organizational Unit")),
-                        array("name"=>"OpenLDAPperson",				"icon"=>"user24.png",		"is_folder"=>false,"display_name"=>gettext("OpenLDAP Person")),
+                        array("name"=>"OpenLDAPorg",				"icon"=>"org.png",		"is_folder"=>true,"display_name"=>gettext("OpenLDAP Organization"),"parent_class"=>"organization"),
+                        array("name"=>"OpenLDAPou",				"icon"=>"folder.png",		"is_folder"=>true,"display_name"=>gettext("OpenLDAP Organizational Unit"),"parent_class"=>"organizationalUnit"),
+                        array("name"=>"OpenLDAPperson",				"icon"=>"user24.png",		"is_folder"=>false,"display_name"=>gettext("OpenLDAP Person"),"parent_class"=>"pilotPerson,inetOrgPerson"),
 			);
 
 		// auxiliary class 'OpenLDAPdisplayableObject' is also defined in this schema
