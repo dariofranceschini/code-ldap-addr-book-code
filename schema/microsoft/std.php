a297dc8d9c6bdcc2c7fd1a9f2522cf3ae231c1e6
James Turner <james@turnersoft.co.uk>
2016-12-23 14:51:07 +0000

    expand comments for microsoft.std.schema

--- schema/microsoft/std.php
+++ schema/microsoft/std.php
@@ -1,12 +1,20 @@
 <?php
-/** microsoft.std.schema (partial) */
+/** Microsoft Standards-Based Schema Definitions (microsoft.std.schema) (partial)
+
+    This schema contains elements of existing/third party standards which
+    were adopted as the basis of the Active Directory schema. Beware that
+    several of these definitions deviate from the RFCs and other standards
+    from which they originate.
+
+    @see:
+	https://tools.ietf.org/html/draft-ietf-asid-schema-pilot-00
+*/
 
 class microsoft_std_schema extends ldap_schema
 {
 	function __construct(&$ldap_server)
 	{
 		$this->attribute_schema = array(
-			// microsoft.std.schema entries (partial)
 			array("name"=>"c",				"data_type"=>"country_code",	"display_name"=>gettext("Country Code")),
 			array("name"=>"cn",				"data_type"=>"text",		"display_name"=>gettext("Common Name/Full Name")),
 			array("name"=>"description",			"data_type"=>"text",		"display_name"=>gettext("Description")),
@@ -25,9 +33,15 @@ class microsoft_std_schema extends ldap_schema
 			array("name"=>"st",				"data_type"=>"text",		"display_name"=>gettext("State (or Province/County)")),
 			array("name"=>"street",				"data_type"=>"text_area",	"display_name"=>gettext("Street Address")),
 			array("name"=>"telephoneNumber",		"data_type"=>"phone_number",	"display_name"=>gettext("Telephone Number")),
-			array("name"=>"thumbnailLogo",			"data_type"=>"image",		"display_name"=>gettext("Thumbnail Logo")),		// non-standard
-			array("name"=>"thumbnailPhoto",			"data_type"=>"image",		"display_name"=>gettext("Thumbnail Photograph")),	// non-standard
 			array("name"=>"title",				"data_type"=>"text",		"display_name"=>gettext("Job Title")),
+
+			// Active Directory uses non-standard OIDs for these attributes instead
+			// of those referenced by the ASID working group. Microsoft use OIDs from
+			// ranges that have been registered to Netscape. The comment after each
+			// definition shows the conflicting Netscape attribute with the same OID.
+
+			array("name"=>"thumbnailLogo",			"data_type"=>"image",		"display_name"=>gettext("Thumbnail Logo")),		// nsLicensedFor
+			array("name"=>"thumbnailPhoto",			"data_type"=>"image",		"display_name"=>gettext("Thumbnail Photograph")),	// changeLog
 
 			// Added for Windows Server 2003 (or inetOrgPerson kit for Windows 2000 Server)
 			array("name"=>"jpegPhoto",			"data_type"=>"image",		"display_name"=>gettext("Photograph")),
@@ -36,13 +50,16 @@ class microsoft_std_schema extends ldap_schema
 		// Structural object classes
 		$this->object_schema = array(
 			// Non-standard implementation of InetOrgPerson (RFC 2798):
-			//	- Subclass of proprietary "user" (so listed before it in schema definition)
+			//	- Subclass of proprietary "user"
 			//	- Attribute "sn" is not mandatory
 			array("name"=>"inetOrgPerson",			"icon"=>"user24.png",		"is_folder"=>false,"display_name"=>gettext("InetOrgPerson"),"can_create"=>true,"parent_class"=>"user"),
 
+			// LDAP Core Schema for User Applications (RFC 2256)
 			array("name"=>"organizationalUnit",		"icon"=>"folder.png",		"is_folder"=>true,"rdn_attrib"=>"ou","display_name"=>gettext("Organizational Unit"),"can_create"=>true),
+
+			// System Schema
 			array("name"=>"subSchema",			"icon"=>"schema.png",		"is_folder"=>false,"display_name"=>gettext("Schema"))
-                        );
+			);
 
 		// Display layouts
 		$ldap_server->add_display_layout("organizationalUnit",array(
