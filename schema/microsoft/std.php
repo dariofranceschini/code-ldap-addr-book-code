78f0136495acc47e8281a4c8988e1a5a2ddb0a50
James Turner <james@turnersoft.co.uk>
2016-12-26 11:53:57 +0000

    list core and COSINE attributes separately in microsoft.std.schema

--- schema/microsoft/std.php
+++ schema/microsoft/std.php
@@ -15,18 +15,16 @@ class microsoft_std_schema extends ldap_schema
 	function __construct(&$ldap_server)
 	{
 		$this->attribute_schema = array(
+			// LDAP Core Schema for User Applications
 			array("name"=>"c",				"data_type"=>"country_code",	"display_name"=>gettext("Country Code")),
 			array("name"=>"cn",				"data_type"=>"text",		"display_name"=>gettext("Common Name/Full Name")),
 			array("name"=>"description",			"data_type"=>"text",		"display_name"=>gettext("Description")),
 			array("name"=>"facsimileTelephoneNumber",	"data_type"=>"text",		"display_name"=>gettext("Fax Number")),
 			array("name"=>"givenName",			"data_type"=>"text",		"display_name"=>gettext("Given Name")),
-			array("name"=>"homePhone",			"data_type"=>"phone_number",	"display_name"=>gettext("Home Telephone Number")),
 			array("name"=>"l",				"data_type"=>"text",		"display_name"=>gettext("Locality (e.g. Town/City)")),
 			array("name"=>"mail",				"data_type"=>"text",		"display_name"=>gettext("E-mail Address")),
 			array("name"=>"member",				"data_type"=>"dn_list",		"display_name"=>gettext("Members")),
-			array("name"=>"mobile",				"data_type"=>"phone_number",	"display_name"=>gettext("Mobile/Cell Telephone Number")),
 			array("name"=>"ou",				"data_type"=>"text",		"display_name"=>gettext("Organizational Unit Name")),
-			array("name"=>"pager",				"data_type"=>"text",		"display_name"=>gettext("Pager Telephone Number")),
 			array("name"=>"physicalDeliveryOfficeName",	"data_type"=>"text",		"display_name"=>gettext("Office")),
 			array("name"=>"postalCode",			"data_type"=>"postcode",	"display_name"=>gettext("Postal Code")),
 			array("name"=>"sn",				"data_type"=>"text",		"display_name"=>gettext("Surname")),
@@ -34,6 +32,11 @@ class microsoft_std_schema extends ldap_schema
 			array("name"=>"street",				"data_type"=>"text_area",	"display_name"=>gettext("Street Address")),
 			array("name"=>"telephoneNumber",		"data_type"=>"phone_number",	"display_name"=>gettext("Telephone Number")),
 			array("name"=>"title",				"data_type"=>"text",		"display_name"=>gettext("Job Title")),
+
+			// COSINE and Internet X.500 Schema
+			array("name"=>"homePhone",			"data_type"=>"phone_number",	"display_name"=>gettext("Home Telephone Number")),
+			array("name"=>"mobile",				"data_type"=>"phone_number",	"display_name"=>gettext("Mobile/Cell Telephone Number")),
+			array("name"=>"pager",				"data_type"=>"text",		"display_name"=>gettext("Pager Telephone Number")),
 
 			// Active Directory uses non-standard OIDs for these attributes instead
 			// of those referenced by the ASID working group. Microsoft use OIDs from
