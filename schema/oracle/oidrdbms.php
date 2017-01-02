c4602878ab8d987595496f290cefd00c0045373e
James Turner <james@turnersoft.co.uk>
2017-01-02 14:37:33 +0000

    add display layout for orclDBServer object class

--- schema/oracle/oidrdbms.php
+++ schema/oracle/oidrdbms.php
@@ -27,7 +27,32 @@ class oracle_oidrdbms_schema extends ldap_schema
 			array("name"=>"orclDBSubtreeLevelMapping",	"icon"=>"generic24.png",			"is_folder"=>false,"display_name"=>gettext("Oracle Database Subtree Level Mapping"))
                         );
 
+                // Display layouts
+                $ldap_server->add_display_layout("orclDBServer",array(
+                        array(
+                                "attributes"=>array(
+                                        array("cn",                     gettext("Database Name"),		"oracle/oracle-dbserver.png"),
+					array("orclSystemName",		gettext("Server Host Name"),		"generic24.png"),
+					array("orclOracleHome",		gettext("Oracle Home"),			"generic24.png"),
+					array("orclSid",		gettext("SID"),				"generic24.png"),
+					array("orclDBGlobalName",	gettext("Global Name"),			"generic24.png"),
+					array("orclNetDescName",	gettext("Net Service Name"),		"generic24.png"),
+                                        )
+                                ),
+                        array("section_name"=>gettext("Connect Descriptor"),"new_row"=>true,
+                                "attributes"=>array(
+                                        array("orclNetDescString")
+                                        )
+                                )
+                        ));
+
 		parent::__construct($ldap_server);
+        }
+
+        function before_create_orclDBServer(&$ldap_server,&$entry)
+        {
+		$this->add_attrib_single_value($ldap_server,$entry,"orclServiceType","DB");	// indicates service is a database
+		$this->add_attrib_single_value($ldap_server,$entry,"orclNetDescName","000:cn=DESCRIPTION_0");
         }
 }
 ?>
