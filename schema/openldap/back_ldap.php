52207013c789dfc7d2bbd1aab3616c97e991ccd4
James Turner <james@turnersoft.co.uk>
2017-01-08 13:44:29 +0000

    define olcDistProcConfig and olmLDAPConnection object classes; reference olmLDAPDatabase

--- schema/openldap/back_ldap.php
+++ schema/openldap/back_ldap.php
@@ -8,8 +8,12 @@ class openldap_back_ldap_schema extends ldap_schema
 		// Structural object classes
 		$this->object_schema = array(
 			array("name"=>"olcLDAPConfig",			"icon"=>"openldap/db.png",		"is_folder"=>false,"rdn_attrib"=>"olcDatabase","display_name"=>gettext("LDAP Proxy Database"),"required_attribs"=>"olcSuffix","parent_class"=>"olcDatabaseConfig"),
-			array("name"=>"olcChainConfig",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Chain Overlay"),"parent_class"=>"olcOverlayConfig")
+			array("name"=>"olcChainConfig",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Chain Overlay"),"parent_class"=>"olcOverlayConfig"),
+			array("name"=>"olcDistProcConfig",		"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Distributed Procedure Overlay"),"parent_class"=>"olcOverlayConfig"),
+			array("name"=>"olmLDAPConnection",		"icon"=>"openldap/connection.png",	"is_folder"=>false,"display_name"=>gettext("Monitored LDAP Proxy Connection"),"parent_class"=>"monitorConnection")
 			);
+
+		// auxiliary class 'olmLDAPDatabase' is also defined in this schema
 
 		parent::__construct($ldap_server);
 	}
