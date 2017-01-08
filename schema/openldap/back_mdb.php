1888760ce81a8fb5cfca80a9e8d010f8f98cbf99
James Turner <james@turnersoft.co.uk>
2017-01-08 12:14:52 +0000

    add definition for olcDbMaxSize attribute

--- schema/openldap/back_mdb.php
+++ schema/openldap/back_mdb.php
@@ -5,6 +5,11 @@ class openldap_back_mdb_schema extends ldap_schema
 {
 	function __construct(&$ldap_server)
 	{
+		// Attributes
+		$this->attribute_schema = array(
+			array("name"=>"olcDbMaxSize",			"data_type"=>"text",		"display_name"=>gettext("Maximum Database Size"))
+			);
+
 		// Structural object classes
 		$this->object_schema = array(
 			array("name"=>"olcMdbConfig",			"icon"=>"openldap/db.png",		"is_folder"=>false,"rdn_attrib"=>"olcDatabase","display_name"=>gettext("MDB Database"),"required_attribs"=>"olcSuffix,olcDbDirectory","parent_class"=>"olcDatabaseConfig")
