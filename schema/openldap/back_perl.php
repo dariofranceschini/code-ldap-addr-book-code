7b06389ca11a36530a7d42ba4376187decb846a6
James Turner <james@turnersoft.co.uk>
2017-01-11 08:34:25 +0000

    add display layout/functions for creating olcPerlConfig objects

--- schema/openldap/back_perl.php
+++ schema/openldap/back_perl.php
@@ -14,10 +14,57 @@ class openldap_back_perl_schema extends ldap_schema
 
 		// Structural object classes
 		$this->object_schema = array(
-			array("name"=>"olcDbPerlConfig",			"icon"=>"openldap/db.png",		"is_folder"=>false,"rdn_attrib"=>"olcDatabase","display_name"=>gettext("Perl Module Database"),"required_attribs"=>"olcSuffix","parent_class"=>"olcDatabaseConfig")
+			array("name"=>"olcDbPerlConfig",			"icon"=>"openldap/db.png",		"is_folder"=>false,"rdn_attrib"=>"olcDatabase","display_name"=>gettext("Perl Module Database"),"required_attribs"=>"olcSuffix","can_create"=>true,"parent_class"=>"olcDatabaseConfig")
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcDbPerlConfig",array(
+			array("section_name"=>gettext("Perl Database Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcSuffix",			gettext("Naming Context"),				"alias.png"),
+					array("olcPerlModule",			gettext("Module Name"),					"generic24.png"),
+					array("olcPerlModulePath",		gettext("Module Path"),					"folder.png"),
+					array("olcPerlModuleConfig",		gettext("Module Config Directives"),			"generic24.png"),
+					array("olcPerlFilterSearchResults",	gettext("Filter Search Results within OpenLDAP"),	"generic24.png"),
+					array("olcLastMod",			gettext("Maintain Last Modification Info"),		"generic24.png"),
+					array("olcRootDN",			gettext("Root User"),					"generic24.png"),
+				//	array("olcRootPW",			gettext("Root Password"),				"generic24.png"),
+					),
+				),
+			array("section_name"=>gettext("Access Controls"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcAccess")
+					),
+				),
+			array("section_name"=>gettext("Overlays"),"new_row"=>true,"colspan"=>2,
+				"attributes"=>array(
+					array("__CHILD_OBJECTS__")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcDbPerlConfig(&$ldap_server,&$entry)
+	{
+		// override the schema-defined data type that the new database's DN can be typed in
+		$ldap_server->modify_attribute_schema("olcSuffix","data_type","text");
+	}
+
+	function before_create_olcDbPerlConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("back_perl");
+
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcDatabaseConfig","perl",-1);
+
+		$this->add_attrib_single_value($ldap_server,$entry,"olcAccess",array(
+			"{0}to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * break",
+			"{1}to * by dn.base=\"" . $_SESSION["LOGIN_BIND_DN"] . "\" manage",
+			"{2}to attrs=userPassword by self write by anonymous auth by * none",
+			"{3}to attrs=shadowLastChange by self write by * read",
+			"{4}to * by * read")
+			);
 	}
 }
 ?>
