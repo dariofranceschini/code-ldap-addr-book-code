94cd28d8a366065406da8dbb1a94d851cdaeb96e
James Turner <james@turnersoft.co.uk>
2017-01-16 14:37:00 +0000

    added display layout/functions for creating olcSssVlvConfig objects

--- schema/openldap/sssvlv.php
+++ schema/openldap/sssvlv.php
@@ -14,10 +14,37 @@ class openldap_sssvlv_schema extends ldap_schema
 
 		// Structural object classes
 		$this->object_schema = array(
-			array("name"=>"olcSssVlvConfig",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Server Side Sorting and Virtual List View Overlay"),"parent_class"=>"olcOverlayConfig")
+			array("name"=>"olcSssVlvConfig",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Server Side Sorting and Virtual List View Overlay"),"can_create"=>true,"parent_class"=>"olcOverlayConfig")
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcSssVlvConfig",array(
+			array("section_name"=>gettext("Server Side Sorting and Virtual List View Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),						"openldap/overlay.png"),
+					array("olcSssVlvMax",			gettext("Maximum Concurrent Sort Requests for All Connections"),	"generic24.png"),
+					array("olcSssVlvMaxKeys",		gettext("Maximum Number of Keys per Sort Request"),			"generic24.png"),
+					array("olcSssVlvMaxPerConn",		gettext("Maximum Concurrent Paged Search Requests per Connection"),	"generic24.png")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcSssVlvConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","sssvlv");
+
+//		$this->add_attrib_value($ldap_server,$entry,"olcSssVlvMax","???");	// default to half number of server threads
+		$this->add_attrib_value($ldap_server,$entry,"olcSssVlvMaxKeys","5");
+		$this->add_attrib_value($ldap_server,$entry,"olcSssVlvMaxPerConn","5");
+
+	}
+
+	function before_create_olcSssVlvConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("sssvlv");
 	}
 }
 ?>
