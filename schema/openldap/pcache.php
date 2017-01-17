7d477c35e31edbc6eb95e3d9e686ed43e0a011c9
James Turner <james@turnersoft.co.uk>
2017-01-17 12:15:52 +0000

    added display layout/create functions for olcPcacheConfig objects

--- schema/openldap/pcache.php
+++ schema/openldap/pcache.php
@@ -26,7 +26,53 @@ class openldap_pcache_schema extends ldap_schema
 
 		// auxiliary class 'olcPcacheDatabase' is also defined in this schema
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcPcacheConfig",array(
+			array("section_name"=>gettext("Proxy Cache Engine Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),				"openldap/overlay.png"),
+					array("olcPcache",			gettext("Cache Parameters"),				"generic24.png"),
+					array("olcPcacheMaxQueries",		gettext("Maximum Number of Cached Queries"),		"generic24.png"),
+					array("olcPcacheOffline",		gettext("Offline Mode"),				"generic24.png"),
+					array("olcPcachePersist",		gettext("Keep Cached Queries When Proxy Restarts"),	"generic24.png"),
+					array("olcPcacheValidate",		gettext("Validate Entries Against Proxy DSA's Schema Before Caching"),"generic24.png"),
+					array("olcPcachePosition",		gettext("Proxy Cache Access Priority"),			"generic24.png"),
+					)
+				),
+			array("section_name"=>gettext("Cached Attribute Sets"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcPcacheAttrset"),
+					)
+				),
+			array("section_name"=>gettext("Caching Templates for Queries"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcPcacheTemplate"),
+					)
+				),
+			array("section_name"=>gettext("Caching Templates for Bind Credentials"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcPcacheBind"),
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcPcacheConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","pcache");
+		$this->add_attrib_value($ldap_server,$entry,"olcPcache","bdb 10000 1 50 100");
+		$this->add_attrib_value($ldap_server,$entry,"olcPcacheMaxQueries","10000");
+		$this->add_attrib_value($ldap_server,$entry,"olcPcacheValidate","FALSE");
+		$this->add_attrib_value($ldap_server,$entry,"olcPcacheOffline","FALSE");
+		$this->add_attrib_value($ldap_server,$entry,"olcPcachePersist","FALSE");
+		$this->add_attrib_value($ldap_server,$entry,"olcPcachePosition","tail");
+	}
+
+	function before_create_olcPcacheConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("pcache");
 	}
 }
 ?>
