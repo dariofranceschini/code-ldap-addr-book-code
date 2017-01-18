b7865fcb548d2a4641225796dec4db7e982c666c
James Turner <james@turnersoft.co.uk>
2017-01-18 12:55:50 +0000

    added display layout/functions for creating olcNssOvConfig objects

--- schema/openldap/nssov.php
+++ schema/openldap/nssov.php
@@ -18,9 +18,62 @@ class openldap_nssov_schema extends ldap_schema
 			array("name"=>"olcNssOvConfig",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("NSS/PAM Lookup Overlay"),"parent_class"=>"olcOverlayConfig")
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcNssOvConfig",array(
+			array("section_name"=>gettext("NSS/PAM Lookup Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),					"openldap/overlay.png"),
+					array("olcNssPamDefHost",		gettext("Default Host Name for Service Checks"),		"server.png"),
+					array("olcNssPamGroupDN",		gettext("Group of Users Allowed to Authenticate"),		"group24.png"),
+					array("olcNssPamGroupAD",		gettext("Group Member Attribute"),				"user24.png"),
+					array("olcNssPamMinUid",		gettext("Minimum UID Value Allowed to Log In"),			"generic24.png"),
+					array("olcNssPamMaxUid",		gettext("Minimum UID Value Allowed to Log In"),			"generic24.png"),
+					array("olcNssPamTemplateAD",		gettext("Template Login Name Attribute"),			"generic24.png"),
+					array("olcNssPamTemplate",		gettext("Login Name to Use if Template Attribute is Empty"),	"generic24.png"),
+
+					// TODO: add password mananager/password policy attributes?
+
+					)
+				),
+			array("section_name"=>gettext("Service Search Descriptors"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcNssSsd")
+					)
+				),
+			array("section_name"=>gettext("Service Attribute Mappings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcNssMap")
+					)
+				),
+			array("section_name"=>gettext("Services with Session Logging"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcNssPamSession")
+					)
+				),
+			array("section_name"=>gettext("PAM Authentication and Authorization Options"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcNssPam")
+					)
+				)
+			));
+
 		$ldap_server->add_schema("openldap/ldapns");
 
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcNssOvConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","nssov");
+
+		$this->add_attrib_value($ldap_server,$entry,"olcNssPamGroupAD","member");
+		$this->add_attrib_value($ldap_server,$entry,"olcNssPamMinUid","0");
+		$this->add_attrib_value($ldap_server,$entry,"olcNssPamMaxUid","0");
+	}
+
+	function before_create_olcNssOvConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("nssov");
 	}
 }
 ?>
