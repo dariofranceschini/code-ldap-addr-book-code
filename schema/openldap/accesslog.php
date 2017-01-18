51b2ff9888995174737040f2378a7c00b609f465
James Turner <james@turnersoft.co.uk>
2017-01-18 13:40:11 +0000

    added display layout/functions for creating olcAccessLogConfig objects

--- schema/openldap/accesslog.php
+++ schema/openldap/accesslog.php
@@ -27,7 +27,7 @@ class openldap_accesslog_schema extends ldap_schema
 
 		// Structural object classes
 		$this->object_schema = array(
-			array("name"=>"olcAccessLogConfig",		"icon"=>"openldap/overlay.png",			"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Access Logging Overlay"),"parent_class"=>"olcOverlayConfig"),
+			array("name"=>"olcAccessLogConfig",		"icon"=>"openldap/overlay.png",			"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Access Logging Overlay"),"can_create"=>true,"create_method"=>"atomic","parent_class"=>"olcOverlayConfig"),
 
 			array("name"=>"auditContainer",			"icon"=>"openldap/audit-container.png",		"is_folder"=>true,"display_name"=>gettext("AuditLog Container")),
 			array("name"=>"auditObject",			"icon"=>"document.png",				"is_folder"=>false,"rdn_attrib"=>"reqStart","display_name"=>gettext("Auditing Event"),"required_attribs"=>"reqType,reqSession"),
@@ -44,7 +44,52 @@ class openldap_accesslog_schema extends ldap_schema
 			array("name"=>"auditExtended",			"icon"=>"config-file.png",			"is_folder"=>false,"rdn_attrib"=>"reqStart","display_name"=>gettext("Extended Operation Event"),"required_attribs"=>"reqType,reqSession")
 			);
 
+		$ldap_server->add_display_layout("olcAccessLogConfig",array(
+			array("section_name"=>gettext("Access Logging Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",		gettext("Overlay Object Name"),					"openldap/overlay.png"),
+
+					// TODO: must be DN of another database's naming context
+					// higher numbered than the one to which the overlay applies???
+					array("olcAccessLogDB",		gettext("Database in Which to Store the Access Log"),		"openldap/db.png"),
+					array("olcAccessLogOps",	gettext("Operation Types to be Logged (Space-Seperated)"),	"generic24.png"),
+					array("olcAccessLogSuccess",	gettext("Only Record Successful Operations"),			"generic24.png"),
+					array("olcAccessLogPurge",	gettext("Remove Old Log Entries (Maximum Age, Cleanup Interval)"),"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Subtree-Specific Logging Options"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcAccessLogBase")
+					)
+				),
+			array("section_name"=>gettext("Log Old Values When Attributes are Modified"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcAccessLogOldAttr",	gettext("Always Log for the Following Attributes"),		"generic24.png"),
+					array("olcAccessLogOld",	gettext("When Objects Matching this LDAP Filter are Modified"),	"generic24.png")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcAccessLogConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","accesslog");
+
+		// override the schema-defined data type that the access log database's DN can be typed in
+		$ldap_server->modify_attribute_schema("olcAccessLogDB","data_type","text");
+
+		$this->add_attrib_value($ldap_server,$entry,"olcAccessLogOps","all");
+		$this->add_attrib_value($ldap_server,$entry,"olcAccessLogOld","(objectClass=*)");
+		$this->add_attrib_value($ldap_server,$entry,"olcAccessLogOldAttr","cn");
+		$this->add_attrib_value($ldap_server,$entry,"olcAccessLogPurge","30+00:00 1+00:00");	// 30 day log retention, housekeeping every 1 day
+		$this->add_attrib_value($ldap_server,$entry,"olcAccessLogSuccess","FALSE");
+	}
+
+	function before_create_olcAccessLogConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("accesslog");
 	}
 }
 ?>
