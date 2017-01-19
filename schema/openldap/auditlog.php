8b6d986f23da6adf19b83d19a7b104445f7d38d3
James Turner <james@turnersoft.co.uk>
2017-01-19 12:02:30 +0000

    added display layout/functions for creating olcAuditlogConfig objects

--- schema/openldap/auditlog.php
+++ schema/openldap/auditlog.php
@@ -11,10 +11,29 @@ class openldap_auditlog_schema extends ldap_schema
 
 		// Structural object classes
 		$this->object_schema = array(
-			array("name"=>"olcAuditlogConfig",		"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Audit Logging Overlay"),"parent_class"=>"olcOverlayConfig")
+			array("name"=>"olcAuditlogConfig",		"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Audit Logging Overlay"),"can_create"=>true,"create_method"=>"atomic","parent_class"=>"olcOverlayConfig")
 			);
 
+		$ldap_server->add_display_layout("olcAuditlogConfig",array(
+			array("section_name"=>gettext("Audit Logging Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),			"openldap/overlay.png"),
+					array("olcAuditlogFile",		gettext("Audit Log File"),			"document.png")
+					),
+				),
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcAuditlogConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","auditlog");
+	}
+
+	function before_create_olcAuditlogConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("auditlog");
 	}
 }
 ?>
