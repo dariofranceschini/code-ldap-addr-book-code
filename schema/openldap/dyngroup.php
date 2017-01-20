e69a862d41d94360569fdee209ed4564b10a7def
James Turner <james@turnersoft.co.uk>
2017-01-20 12:41:57 +0000

    added display layout/functions for creating olcDGConfig objects

--- schema/openldap/dyngroup.php
+++ schema/openldap/dyngroup.php
@@ -14,7 +14,28 @@ class openldap_dyngroup_schema extends ldap_schema
 			array("name"=>"olcDGConfig",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Dynamic Groups Overlay"),"parent_class"=>"olcOverlayConfig")
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcDGConfig",array(
+			array("section_name"=>gettext("Dynamic Groups Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),			"openldap/overlay.png"),
+					array("olcDGAttrPair",			gettext("Member/Member URL Attribute Pair"),	"generic24.png")
+					),
+				),
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcDGConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","dyngroup");
+		$this->add_attrib_value($ldap_server,$entry,"olcDGAttrPair","member memberURL");
+	}
+
+	function before_create_olcDGConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("dyngroup");
 	}
 }
 ?>
