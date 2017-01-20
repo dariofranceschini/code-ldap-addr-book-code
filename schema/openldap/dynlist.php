eae3f85387cdb7d4e3eb68c274fbfbf6f4e4e337
James Turner <james@turnersoft.co.uk>
2017-01-20 12:47:50 +0000

    added display layout/functions for creating olcDynamicList objects

--- schema/openldap/dynlist.php
+++ schema/openldap/dynlist.php
@@ -14,7 +14,28 @@ class openldap_dynlist_schema extends ldap_schema
 			array("name"=>"olcDynamicList",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Dynamic Lists Overlay"),"parent_class"=>"olcOverlayConfig")
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcDynamicList",array(
+			array("section_name"=>gettext("Dynamic Lists Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),				"openldap/overlay.png"),
+				//	array("olcDlAttrSet",			gettext("Member/Member URL Attribute Pair"),		"generic24.png")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcDynamicList(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","dynlist");
+	//	$this->add_attrib_value($ldap_server,$entry,"olcDlAttrSet","groupOfURLs memberURL");
+	}
+
+	function before_create_olcDynamicList(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("dynlist");
 	}
 }
 ?>
