bb4a978b1c81d88b6c02cde415d91cf6cc8d5779
James Turner <james@turnersoft.co.uk>
2017-01-20 12:06:56 +0000

    added display layout/functions for creating olcConstraintConfig objects

--- schema/openldap/constraint.php
+++ schema/openldap/constraint.php
@@ -14,7 +14,31 @@ class openldap_constraint_schema extends ldap_schema
 			array("name"=>"olcConstraintConfig",		"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Constraints Overlay"),"parent_class"=>"olcOverlayConfig")
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcConstraintConfig",array(
+			array("section_name"=>gettext("Constraints Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),			"openldap/overlay.png")
+					)
+				),
+			array("section_name"=>gettext("Attribute Constraints"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcConstraintAttribute")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcConstraintConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","constraint");
+	}
+
+	function before_create_olcConstraintConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("constraint");
 	}
 }
 ?>
