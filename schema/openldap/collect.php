c1fbd725aca3976abc3c22def279c69f8f960706
James Turner <james@turnersoft.co.uk>
2017-01-20 10:26:24 +0000

    added display layout/functions for creating olcCollectConfig objects

--- schema/openldap/collect.php
+++ schema/openldap/collect.php
@@ -11,10 +11,30 @@ class openldap_collect_schema extends ldap_schema
 
 		// Structural object classes
 		$this->object_schema = array(
-			array("name"=>"olcCollectConfig",		"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Collective Attributes Overlay"),"parent_class"=>"olcOverlayConfig")
+			array("name"=>"olcCollectConfig",		"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Collective Attributes Overlay"),"can_create"=>true,"parent_class"=>"olcOverlayConfig"),
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcCollectConfig",array(
+			array("section_name"=>gettext("Collective Attributes Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),				"openldap/overlay.png"),
+					array("olcCollectInfo",			gettext("Ancestor DN/Collective Attributes"),		"document.png")
+					),
+				),
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcCollectConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","collect");
+	}
+
+	function before_create_olcCollectConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("collect");
 	}
 }
 ?>
