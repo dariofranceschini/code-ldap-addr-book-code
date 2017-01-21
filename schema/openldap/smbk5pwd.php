73ac4587e4e4ba2eab52e347d95f7aee85a4949d
James Turner <james@turnersoft.co.uk>
2017-01-21 13:50:46 +0000

    added display layout/functions for creating olcSmbK5PwdConfig objects

--- schema/openldap/smbk5pwd.php
+++ schema/openldap/smbk5pwd.php
@@ -16,7 +16,38 @@ class openldap_smbk5pwd_schema extends ldap_schema
 			array("name"=>"olcSmbK5PwdConfig",		"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Samba/Kerberos Password Management Overlay"),"can_create"=>true,"parent_class"=>"olcOverlayConfig")
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcSmbK5PwdConfig",array(
+			array("section_name"=>gettext("Samba/Kerberos 5 Password Management Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),				"openldap/overlay.png"),
+					array("olcSmbK5PwdMustChange",		gettext("Maximum Password Age (Seconds)"),		"generic24.png"),
+					array("olcSmbK5PwdCanChange",		gettext("Minimum Password Age (seconds)"),		"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Enabled Password Modifier Modules"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcSmbK5PwdEnable")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcSmbK5PwdConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","smbk5pwd");
+	//	$this->add_attrib_value($ldap_server,$entry,"olcSmbK5PwdMustChange","???");
+	//	$this->add_attrib_value($ldap_server,$entry,"olcSmbK5PwdCanChange","???");
+	}
+
+	function before_create_olcSmbK5PwdConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("smbk5pwd");
+
+		$this->add_attrib_single_value($ldap_server,$entry,"olcSmbK5PwdEnable",
+			array("krb5","samba"));
 	}
 }
 ?>
