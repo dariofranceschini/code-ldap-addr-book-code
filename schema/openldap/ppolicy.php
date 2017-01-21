efaa8a2593933792855ed70712078393cadeb0e7
James Turner <james@turnersoft.co.uk>
2017-01-21 13:02:14 +0000

    added display layout/functions for creating olcPPolicyConfig objects

--- schema/openldap/ppolicy.php
+++ schema/openldap/ppolicy.php
@@ -23,10 +23,36 @@ class openldap_ppolicy_schema extends ldap_schema
 
 		// Structural object classes
 		$this->object_schema = array(
-			array("name"=>"olcPPolicyConfig",		"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Password Policies Overlay"),"parent_class"=>"olcOverlayConfig")
+			array("name"=>"olcPPolicyConfig",		"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Password Policies Overlay"),"can_create"=>true,"parent_class"=>"olcOverlayConfig")
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcPPolicyConfig",array(
+			array("section_name"=>gettext("Password Policies Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),				"openldap/overlay.png"),
+					array("olcPPolicyDefault",		gettext("Default Policy Object"),			"generic24.png"),
+					array("olcPPolicyHashCleartext",	gettext("Hash Passwords on Add/Modify"),		"generic24.png"),
+					array("olcPPolicyForwardUpdates",	gettext("Forward Proxy State Updates to Master"),	"generic24.png"),
+					array("olcPPolicyUseLockout",		gettext("Inform Clients when Account Locked Out"),	"generic24.png")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcPPolicyConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","ppolicy");
+		$this->add_attrib_value($ldap_server,$entry,"olcPPolicyHashCleartext","FALSE");
+		$this->add_attrib_value($ldap_server,$entry,"olcPPolicyForwardUpdates","FALSE");
+		$this->add_attrib_value($ldap_server,$entry,"olcPPolicyUseLockout","FALSE");
+	}
+
+	function before_create_olcPPolicyConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("ppolicy");
 	}
 }
 ?>
