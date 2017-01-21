f705bc0143ed25ad566d48a4a432877129471a66
James Turner <james@turnersoft.co.uk>
2017-01-21 14:25:08 +0000

    added display layout/functions for creating olcValSortConfig objects

--- schema/openldap/valsort.php
+++ schema/openldap/valsort.php
@@ -14,7 +14,28 @@ class openldap_valsort_schema extends ldap_schema
 			array("name"=>"olcValSortConfig",		"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Value Sorting Overlay"),"can_create"=>true,"required_attribs"=>"olcValSortAttr","parent_class"=>"olcOverlayConfig")
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcValSortConfig",array(
+			array("section_name"=>gettext("Value Sorting Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),				"openldap/overlay.png"),
+					array("olcValSortAttr",			gettext("Value Sorting Configuration"),			"generic24.png"),
+					),
+				),
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcValSortConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","valsort");
+		// TODO: fill in olcValSortAttr with "member <db>" where <db> is the underlying database's naming context DN
+	}
+
+	function before_create_olcValSortConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("valsort");
 	}
 }
 ?>
