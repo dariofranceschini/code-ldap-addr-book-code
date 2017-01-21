38a57c0a72bc2817a2d97a7d08ba3ab282294319
James Turner <james@turnersoft.co.uk>
2017-01-21 13:13:09 +0000

    added display layout and functions for creating olcRefintConfig objects

--- schema/openldap/refint.php
+++ schema/openldap/refint.php
@@ -13,10 +13,32 @@ class openldap_refint_schema extends ldap_schema
 
 		// Structural object classes
 		$this->object_schema = array(
-			array("name"=>"olcRefintConfig",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Referential Integrity Overlay"),"parent_class"=>"olcOverlayConfig")
+			array("name"=>"olcRefintConfig",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Referential Integrity Overlay"),"parent_class"=>"olcOverlayConfig","can_create"=>true)
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcRefintConfig",array(
+			array("section_name"=>gettext("Referential Integrity Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),					"openldap/overlay.png"),
+					array("olcRefintAttribute",		gettext("Maintain Referential Integrity for These Attributes"),	"generic24.png"),
+					array("olcRefintNothing",		gettext("Placeholder Value for Empty Mandatory Attributes"),	"generic24.png"),
+					array("olcRefintModifiersName",		gettext("Modifier's Name to be Used"),				"generic24.png"),
+					),
+				),
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcRefintConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","refint");
+	}
+
+	function before_create_olcRefintConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("refint");
 	}
 }
 ?>
