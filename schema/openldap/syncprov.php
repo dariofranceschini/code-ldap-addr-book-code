011db9b460207e4e94cd2c076ef8cc43c3c58832
James Turner <james@turnersoft.co.uk>
2017-01-21 13:59:36 +0000

    added display layout/functions for creating olcSyncProvConfig objects

--- schema/openldap/syncprov.php
+++ schema/openldap/syncprov.php
@@ -14,10 +14,35 @@ class openldap_syncprov_schema extends ldap_schema
 
 		// Structural object classes
 		$this->object_schema = array(
-			array("name"=>"olcSyncProvConfig",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Sync Provider Overlay"),"parent_class"=>"olcOverlayConfig")
+			array("name"=>"olcSyncProvConfig",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Sync Provider Overlay"),"parent_class"=>"olcOverlayConfig","can_create"=>true,"create_method"=>"atomic")
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcSyncProvConfig",array(
+			array("section_name"=>gettext("Sync Provider Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),				"openldap/overlay.png"),
+					array("olcSpCheckpoint",		gettext("Checkpoint Interval (Operations, Minutes)"),	"generic24.png"),
+					array("olcSpSessionlog",		gettext("In-Memory Session Log Size (Operations)"),	"generic24.png"),
+					array("olcSpNoPresent",			gettext("Skip 'Present' Phase of Sync Refresh"),	"generic24.png"),
+					array("olcSpReloadHint",		gettext("Observe Reload Hint in Request Control"),	"generic24.png")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcSyncProvConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","syncprov");
+		$this->add_attrib_value($ldap_server,$entry,"olcSpNoPresent","FALSE");
+		$this->add_attrib_value($ldap_server,$entry,"olcSpReloadHint","FALSE");
+	}
+
+	function before_create_olcSyncProvConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("syncprov");
 	}
 }
 ?>
