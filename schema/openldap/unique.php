50123d057dcebaa316be85a7a2fded035a0e8e0b
James Turner <james@turnersoft.co.uk>
2017-01-21 14:20:16 +0000

    added display layout/functions for creating olcUniqueConfig objects

--- schema/openldap/unique.php
+++ schema/openldap/unique.php
@@ -20,7 +20,35 @@ class openldap_unique_schema extends ldap_schema
 			array("name"=>"olcUniqueConfig",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Uniqueness Overlay"),"parent_class"=>"olcOverlayConfig")
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcUniqueConfig",array(
+			array("section_name"=>gettext("Uniqueness Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),			"openldap/overlay.png"),
+					array("olcUniqueURI",			gettext("Attribute Uniqueness Configuration"),	"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Legacy Configuration Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcUniqueBase",			gettext("Base DN"),				"generic24.png"),
+					array("olcUniqueAttribute",		gettext("Attributes that Must be Unique"),	"generic24.png"),
+					array("olcUniqueIgnore",		gettext("Attributes to Ignore"),		"generic24.png"),
+					array("olcUniqueStrict",		gettext("Enforce Uniqueness for Null Values"),	"generic24.png")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcUniqueConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","unique");
+	}
+
+	function before_create_olcUniqueConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("unique");
 	}
 }
 ?>
