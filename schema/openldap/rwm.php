dc6953ccb97d48b959e121d10b078f3933db69c3
James Turner <james@turnersoft.co.uk>
2017-01-21 13:43:45 +0000

    added display layout/functions for creating olcRwmConfig objects

--- schema/openldap/rwm.php
+++ schema/openldap/rwm.php
@@ -18,7 +18,45 @@ class openldap_rwm_schema extends ldap_schema
 			array("name"=>"olcRwmConfig",			"icon"=>"openldap/overlay.png",		"is_folder"=>false,"rdn_attrib"=>"olcOverlay","display_name"=>gettext("Rewrite/Remap Overlay"),"parent_class"=>"olcOverlayConfig")
 			);
 
+		// Display layouts
+		$ldap_server->add_display_layout("olcRwmConfig",array(
+			array("section_name"=>gettext("Rewrite/Remap Overlay Settings"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcOverlay",			gettext("Overlay Object Name"),				"openldap/overlay.png"),
+					array("olcRwmTFSupport",		gettext("True/False Filter Support"),			"generic24.png"),
+					array("olcRwmNormalizeMapped",		gettext("Normalise Mapped Attribute/Object Classes"),	"generic24.png"),
+					array("olcRwmDropUnrequested",		gettext("Drop Attributes Not Explicitly Requested"),	"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Attribute/Object Class Mapping Rules"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcRwmMap")
+					)
+				),
+			array("section_name"=>gettext("String Rewriting Rules"),"new_row"=>true,
+				"attributes"=>array(
+					array("olcRwmRewrite")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_olcRwmConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->assign_ordered_sequence_rdn($entry,"olcOverlayConfig","rwm");
+		$this->add_attrib_value($ldap_server,$entry,"olcRwmNormalizeMapped","FALSE");
+		$this->add_attrib_value($ldap_server,$entry,"olcRwmDropUnrequested","TRUE");
+
+		// Supported by OpenLDAP, but not necessarily supported by remote proxy/metadirectory
+		// backends.
+		$this->add_attrib_value($ldap_server,$entry,"olcRwmTFSupport","FALSE");
+	}
+
+	function before_create_olcRwmConfig(&$ldap_server,&$entry)
+	{
+		$ldap_server->ensure_openldap_module_loaded("rwm");
 	}
 }
 ?>
