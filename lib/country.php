753904b595397da4d49f75d701d8ca371d83fd63
James Turner <james@turnersoft.co.uk>
2017-04-09 11:40:40 +0100

    add support for WIPO ST.3 country code standard

--- lib/country.php
+++ lib/country.php
@@ -22,6 +22,7 @@
 	http://www.iso.org/iso/country_codes
 	https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
 	https://en.wikipedia.org/wiki/ISO_3166-3
+	http://www.wipo.int/export/sites/www/standards/en/pdf/03-03-01.pdf
 */
 
 /** Officially assigned ISO 3166-1 alpha-2 country codes/names */
@@ -414,6 +415,243 @@ $iso3166_3_former_country_name=array(
 	"ZR" => "Zaire",				// also ISO 3166-1 transitional reservation
 	);
 
+/** WIPO ST.3 (November 2016 ed.) country codes/names */
+
+$wipo_st3_country_name=array(
+	"AD" => "Andorra",
+	"AE" => "United Arab Emirates",
+	"AF" => "Afghanistan",
+	"AG" => "Antigua and Barbuda",
+	"AI" => "Anguilla",
+	"AL" => "Albania",
+	"AM" => "Armenia",
+	"AO" => "Angola",
+	"AP" => "African Regional Industrial Property Organization",	// ISO have agreed not to use this code
+	"AR" => "Argentina",
+	"AT" => "Austria",
+	"AU" => "Australia",
+	"AW" => "Aruba",
+	"AZ" => "Azerbaijan",
+	"BA" => "Bosnia and Herzegovina",
+	"BB" => "Barbados",
+	"BD" => "Bangladesh",
+	"BE" => "Belgium",
+	"BF" => "Burkina Faso",
+	"BG" => "Bulgaria",
+	"BH" => "Bahrain",
+	"BI" => "Burundi",
+	"BJ" => "Benin",
+	"BM" => "Bermuda",
+	"BN" => "Brunei Darussalam",
+	"BO" => "Bolivia, Plurinational State of",
+	"BQ" => "Bonaire, Sint Eustatius and Saba",
+	"BR" => "Brazil",
+	"BS" => "Bahamas",
+	"BT" => "Bhutan",
+	"BV" =>	"Bouvet Island",
+	"BW" => "Botswana",
+	"BX" => "Benelux Trademarks and Design Offices",		// ISO have agreed not to use this code
+	"BY" => "Belarus",
+	"BZ" => "Belize",
+	"CA" => "Canada",
+	"CD" => "Congo, The Democratic Republic of the",
+	"CF" => "Central African Republic",
+	"CG" => "Congo",
+	"CH" => "Switzerland",
+	"CI" => "Côte d'Ivoire",
+	"CK" => "Cook Islands",
+	"CL" => "Chile",
+	"CM" => "Cameroon",
+	"CN" => "China",
+	"CO" => "Colombia",
+	"CR" => "Costa Rica",
+	"CU" => "Cuba",
+	"CV" => "Cabo Verde",		// named as per WIPO ST.3, a.k.a. "Cape Verde"
+	"CW" => "Curaçao",
+	"CY" => "Cyprus",
+	"CZ" => "Czechia",		// named as per WIPO ST.3, a.k.a. "Czech Republic"
+	"DE" => "Germany",
+	"DJ" => "Djibouti",
+	"DK" => "Denmark",
+	"DM" => "Dominica",
+	"DO" => "Dominican Republic",
+	"DZ" => "Algeria",
+	"EA" => "Eurasian Patent Organization",		// previously assigned code "EV"?
+	"EC" => "Ecuador",
+	"EE" => "Estonia",
+	"EG" => "Egypt",
+	"EH" => "Western Sahara",
+	"EM" => "European Union Intellectual Property Office",	// ISO have agreed not to use this code
+	"EP" => "European Patent Office",	// ISO have agreed not to use this code
+	"ER" => "Eritrea",
+	"ES" => "Spain",
+	"ET" => "Ethiopia",
+	"FI" => "Finland",
+	"FJ" => "Fiji",
+	"FK" => "Falkland Islands (Malvinas)",
+	"FO" => "Faroe Islands",
+	"FR" => "France",
+	"GA" => "Gabon",
+	"GB" => "United Kingdom",
+	"GC" => "Patent Office of the Cooperation Council for the Arab States of the Gulf (GCC)",
+								// ISO have agreed not to use this code
+	"GD" => "Grenada",
+	"GE" => "Georgia",
+	"GG" => "Guernsey",
+	"GH" => "Ghana",
+	"GI" => "Gibraltar",
+	"GL" => "Greenland",
+	"GM" => "Gambia",
+	"GN" => "Guinea",
+	"GQ" => "Equatorial Guinea",
+	"GR" => "Greece",
+	"GS" => "South Georgia and the South Sandwich Islands",
+	"GT" => "Guatemala",
+	"GW" => "Guinea-Bissau",
+	"GY" => "Guyana",
+	"HK" => "Hong Kong Special Administrative Region of the People's Republic of China",
+	"HN" => "Honduras",
+	"HR" => "Croatia",
+	"HT" => "Haiti",
+	"HU" => "Hungary",
+	"IB" => "International Bureau of WIPO",		// ISO have agreed not to use this code
+	"ID" => "Indonesia",
+	"IE" => "Ireland",
+	"IL" => "Israel",
+	"IM" => "Isle of Man",
+	"IN" => "India",
+	"IQ" => "Iraq",
+	"IR" => "Iran, Islamic Republic of",
+	"IS" => "Iceland",
+	"IT" => "Italy",
+	"JE" => "Jersey",
+	"JM" => "Jamaica",
+	"JO" => "Jordan",
+	"JP" => "Japan",
+	"KE" => "Kenya",
+	"KG" => "Kyrgyzstan",
+	"KH" => "Cambodia",
+	"KI" => "Kiribati",
+	"KM" => "Comoros",
+	"KN" => "Saint Kitts and Nevis",
+	"KP" => "Korea, Democratic People's Republic of",	// a.k.a. North Korea
+	"KR" => "Korea, Republic of",				// a.k.a. South Korea
+	"KW" => "Kuwait",
+	"KY" => "Cayman Islands",
+	"KZ" => "Kazakstan",
+	"LA" => "Lao People's Democratic Republic",
+	"LB" => "Lebanon",
+	"LC" => "Saint Lucia",
+	"LI" => "Liechtenstein",
+	"LK" => "Sri Lanka",
+	"LR" => "Liberia",
+	"LS" => "Lesotho",
+	"LT" => "Lithuania",
+	"LU" => "Luxembourg",
+	"LV" => "Latvia",
+	"LY" => "Libya",
+	"MA" => "Morocco",
+	"MC" => "Monaco",
+	"MD" => "Moldova, Republic of",
+	"ME" => "Montenegro",
+	"MG" => "Madagascar",
+	"MK" => "Macedonia, former Yugoslav Republic of",	// named as per WIPO ST.3 (disputed)
+	"ML" => "Mali",
+	"MM" => "Myanmar",
+	"MN" => "Mongolia",
+	"MO" => "Macao",
+	"MP" => "Northern Mariana Islands",
+	"MR" => "Mauritania",
+	"MS" => "Montserrat",
+	"MT" => "Malta",
+	"MU" => "Mauritius",
+	"MV" => "Maldives",
+	"MW" => "Malawi",
+	"MX" => "Mexico",
+	"MY" => "Malaysia",
+	"MZ" => "Mozambique",
+	"NA" => "Namibia",
+	"NE" => "Niger",
+	"NG" => "Nigeria",
+	"NI" => "Nicaragua",
+	"NL" => "Netherlands",
+	"NO" => "Norway",
+	"NP" => "Nepal",
+	"NR" => "Nauru",
+	"NZ" => "New Zealand",
+	"OA" => "African Intellectual Property Organization", // ISO have agreed not to use this code
+	"OM" => "Oman",
+	"PA" => "Panama",
+	"PE" => "Peru",
+	"PG" => "Papua New Guinea",
+	"PH" => "Philippines",
+	"PK" => "Pakistan",
+	"PL" => "Poland",
+	"PT" => "Portugal",
+	"PW" => "Palau",
+	"PY" => "Paraguay",
+	"QA" => "Qatar",
+	"QZ" => "Community Plant Variety Office (European Union)",
+	"RO" => "Romania",
+	"RS" => "Serbia",
+	"RU" => "Russian Federation",	// formerly assigned to Barundi
+	"RW" => "Rwanda",
+	"SA" => "Saudi Arabia",
+	"SB" => "Solomon Islands",
+	"SC" => "Seychelles",
+	"SD" => "Sudan",
+	"SE" => "Sweden",
+	"SG" => "Singapore",
+	"SH" => "Saint Helena, Ascension and Tristan da Cunha",
+	"SI" => "Slovenia",
+	"SK" => "Slovakia",		// formerly assigned to Sikkim
+	"SL" => "Sierra Leone",
+	"SM" => "San Marino",
+	"SN" => "Senegal",
+	"SO" => "Somalia",
+	"SR" => "Suriname",
+	"SS" => "South Sudan",
+	"ST" => "São Tomé and Príncipe",
+	"SV" => "El Salvador",
+	"SX" => "Sint Maarten (Dutch part)",
+	"SY" => "Syrian Arab Republic",
+	"SZ" => "Swaziland",
+	"TC" => "Turks and Caicos Islands",
+	"TD" => "Chad",
+	"TG" => "Togo",
+	"TH" => "Thailand",
+	"TJ" => "Tajikistan",
+	"TL" => "Timor-Leste",
+	"TM" => "Turkmenistan",
+	"TN" => "Tunisia",
+	"TO" => "Tonga",
+	"TR" => "Turkey",
+	"TT" => "Trinidad and Tobago",
+	"TV" => "Tuvalu",
+	"TW" => "Taiwan (Province of China)",		// named as per WIPO ST.3 (disputed)
+	"TZ" => "Tanzania, United Republic of",
+	"UA" => "Ukraine",
+	"UG" => "Uganda",
+	"US" => "United States",
+	"UY" => "Uruguay",
+	"UZ" => "Uzbekistan",
+	"VA" => "Holy See (Vatican City State)",
+	"VC" => "Saint Vincent and the Grenadines",
+	"VE" => "Venezuela, Bolivarian Republic of",
+	"VG" => "Virgin Islands, British",
+	"VN" => "Viet Nam",		// named as per WIPO ST.3, also spelt "Vietnam"
+	"VU" => "Vanuatu",
+	"WO" => "World Intellectual Property Organization", // ISO have agreed not to use this code
+	"WS" => "Samoa",
+	"XN" => "Nordic Patent Institute",
+	"XU" => "International Union for the Protection of New Varieties of Plants",
+	"XV" => "Visegrad Patent Institute",
+	"YE" => "Yemen",
+	"ZA" => "South Africa",
+	"ZM" => "Zambia",
+	"ZW" => "Zimbabwe",
+	);
+
 $country_name=array();
 
 function init_country_names()
@@ -424,7 +662,8 @@ function init_country_names()
 		$iso3166_1_exceptional_country_name,
 		$iso3166_1_transitional_country_name,
 		$iso3166_1_indeterminate_country_name,
-		$iso3166_3_former_country_name;
+		$iso3166_3_former_country_name,
+		$wipo_st3_country_name;
 
 	if(!isset($country_code_standard))
 		$country_code_standard="official";
@@ -461,6 +700,11 @@ function init_country_names()
 			case "former":
 				$country_name=array_merge(
 					$iso3166_3_former_country_name,
+					$country_name);
+				break;
+			case "wipo":
+				$country_name=array_merge(
+					$wipo_st3_country_name,
 					$country_name);
 				break;
 			default:
