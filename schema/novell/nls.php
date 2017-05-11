9b9ea717c4e6071f18e29ae5001b380f5c16fb37
James Turner <james@turnersoft.co.uk>
2017-05-11 07:44:02 +0100

    sort NLS object class definitions alphabetically

--- schema/novell/nls.php
+++ schema/novell/nls.php
@@ -8,9 +8,9 @@ class novell_nls_schema extends ldap_schema
 		// Structural object classes
 
 		$this->object_schema = array(
+			array("name"=>"nLSLicenseCertificate",		"icon"=>"novell/license-cert.png",	"is_folder"=>false,"rdn_attrib"=>"nLSLicenseID","display_name"=>gettext("License Certificate")),
 			array("name"=>"nLSLicenseServer",		"icon"=>"novell/license-server.png",	"is_folder"=>false,"display_name"=>gettext("License Service Provider")),
 			array("name"=>"nLSProductContainer",		"icon"=>"novell/license-container.png",	"is_folder"=>true,"rdn_attrib"=>"nLSPublisher,nLSProduct,nLSVersion","required_attribs"=>"nLSRevision","display_name"=>gettext("License Product Container")),
-			array("name"=>"nLSLicenseCertificate",		"icon"=>"novell/license-cert.png",	"is_folder"=>false,"rdn_attrib"=>"nLSLicenseID","display_name"=>gettext("License Certificate")),
 			);
 
 		// Display layouts
