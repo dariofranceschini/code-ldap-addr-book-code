cf13104ea3a44e94b0d4db2488cb11730c49aa8c
James Turner <james@turnersoft.co.uk>
2017-05-15 05:50:28 +0100

    add xTier schema attributes

--- schema/novell/xtier.php
+++ schema/novell/xtier.php
@@ -5,6 +5,13 @@ class novell_xtier_schema extends ldap_schema
 {
 	function __construct(&$ldap_server)
 	{
+		// Attributes
+		$this->attribute_schema = array(
+			array("name"=>"xTier-iFolderPassphrase",	"data_type"=>"text"),
+			array("name"=>"xTier-LocationList",		"data_type"=>"dn_list"),
+			array("name"=>"xTier-URL",			"data_type"=>"text")
+			);
+
 		// Structural object classes
 		$this->object_schema = array(
 			array("name"=>"xTier-StorageLocation",		"icon"=>"novell/xtier-storage-loc.png","is_folder"=>false),
