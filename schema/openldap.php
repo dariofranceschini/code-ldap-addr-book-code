a0a0c1d8b9ac3ba7ec87639507b468047812a04f
James Turner <james@turnersoft.co.uk>
2017-05-21 10:28:56 +0100

    whitespace fixup

--- schema/openldap.php
+++ schema/openldap.php
@@ -5,11 +5,11 @@ class openldap_schema extends ldap_schema
 {
 	function __construct(&$ldap_server)
 	{
-                // Object classes
-                $this->object_schema = array(
-                        array("name"=>"OpenLDAPorg",				"icon"=>"org.png",		"is_folder"=>true,"display_name"=>gettext("OpenLDAP Organization"),"parent_class"=>"organization"),
-                        array("name"=>"OpenLDAPou",				"icon"=>"folder.png",		"is_folder"=>true,"display_name"=>gettext("OpenLDAP Organizational Unit"),"parent_class"=>"organizationalUnit"),
-                        array("name"=>"OpenLDAPperson",				"icon"=>"user24.png",		"is_folder"=>false,"display_name"=>gettext("OpenLDAP Person"),"parent_class"=>"pilotPerson,inetOrgPerson"),
+		// Object classes
+		$this->object_schema = array(
+			array("name"=>"OpenLDAPorg",				"icon"=>"org.png",		"is_folder"=>true,"display_name"=>gettext("OpenLDAP Organization"),"parent_class"=>"organization"),
+			array("name"=>"OpenLDAPou",				"icon"=>"folder.png",		"is_folder"=>true,"display_name"=>gettext("OpenLDAP Organizational Unit"),"parent_class"=>"organizationalUnit"),
+			array("name"=>"OpenLDAPperson",				"icon"=>"user24.png",		"is_folder"=>false,"display_name"=>gettext("OpenLDAP Person"),"parent_class"=>"pilotPerson,inetOrgPerson"),
 			);
 
 		// auxiliary class 'OpenLDAPdisplayableObject' is also defined in this schema
