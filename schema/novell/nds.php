7db468e9623f2e12add3f1f072103f1c73650260
James Turner <james@turnersoft.co.uk>
2017-05-22 09:34:01 +0100

    move treeRoot display layout to NDS schema

--- schema/novell/nds.php
+++ schema/novell/nds.php
@@ -48,6 +48,97 @@ class novell_nds_schema extends ldap_schema
 				),
 			));
 
+		$ldap_server->add_display_layout("treeRoot",array(
+			array("section_name"=>gettext("eDirectory Tree"),"new_row"=>true,"colspan"=>6,
+				"attributes"=>array(
+					array("directoryTreeName",		null,				"novell/tree-root.png")
+					)
+				),
+			array("section_name"=>gettext("Server Details"),"new_row"=>true,"colspan"=>6,
+				"attributes"=>array(
+					array("dSAName",			gettext("LDAP Server"),			"ldap-server.png"),
+					array("vendorVersion",			gettext("Product Version"),		"generic24.png"),
+					array("vendorName",			gettext("Vendor"),			"generic24.png"),
+					array("supportedLDAPVersion",		gettext("Supported LDAP Versions"),	"generic24.png"),
+					array("namingContexts",			gettext("Naming Contexts"),		"alias.png"),
+					array("subschemaSubentry",		gettext("Subschema Subentry"),		"alias.png"),
+					)
+				),
+			array("section_name"=>gettext("Replication"),"new_row"=>true,
+				"attributes"=>array(
+					array("repUpdatesOut",			gettext("Updates Out")),
+					array("repUpdatesIn",			gettext("Updates In")),
+					)
+				),
+			array("section_name"=>gettext("LDAP Binds"),
+				"attributes"=>array(
+					array("strongAuthBinds",		gettext("Strong Auth")),
+					array("simpleAuthBinds",		gettext("Simple Auth")),
+					array("unauthBinds",			gettext("Anonymous")),
+					array("bindSecurityErrors",		gettext("Security Errors")),
+					)
+				),
+			array("section_name"=>gettext("Data Transfer"),
+				"attributes"=>array(
+					array("outBytes",			gettext("Bytes Out")),
+					array("inBytes",			gettext("Bytes In"))
+					)
+				),
+			array("section_name"=>gettext("Ops Processed"),
+				"attributes"=>array(
+					array("extendedOps",			gettext("Extended")),
+					array("abandonOps",			gettext("Abandoned")),
+					array("wholeSubtreeSearchOps",		gettext("Subtree Search")),
+					array("oneLevelSearchOps",		gettext("Single Level Search")),
+					array("searchOps",			gettext("Search")),
+					array("listOps",			gettext("List")),
+					array("modifyRDNOps",			gettext("Modify RDN")),
+					array("removeEntryOps",			gettext("Remove Entry")),
+					array("addEntryOps",			gettext("Add Entry")),
+					array("compareOps",			gettext("Compare")),
+					array("readOps",			gettext("Read")),
+					array("inOps",				gettext("Total In"))
+					)
+				),
+			array("section_name"=>gettext("LDAP Referral Processed"),
+				"attributes"=>array(
+					array("chainings",			gettext("Outbound Referrals Sent")),
+					array("referralsReturned",		gettext("Inbound Referrals Returned")),
+					)
+				),
+			array("section_name"=>gettext("Miscellaneous"),
+				"attributes"=>array(
+					array("errors",				gettext("Errors")),
+					array("securityErrors",			gettext("Security Errors")),
+					)
+				),
+			array("section_name"=>gettext("Supported Grouping Types"),"new_row"=>true,"colspan"=>6,
+				"attributes"=>array(
+					array("supportedGroupingTypes")
+					)
+				),
+			array("section_name"=>gettext("Supported Controls"),"new_row"=>true,"colspan"=>6,
+				"attributes"=>array(
+					array("supportedControl")
+					)
+				),
+			array("section_name"=>gettext("Supported Features"),"new_row"=>true,"colspan"=>6,
+				"attributes"=>array(
+					array("supportedFeatures")
+					)
+				),
+			array("section_name"=>gettext("Supported SASL Mechanisms"),"new_row"=>true,"colspan"=>6,
+				"attributes"=>array(
+					array("supportedSASLMechanisms")
+					)
+				),
+			array("section_name"=>gettext("Supported Extended Operations"),"new_row"=>true,"colspan"=>6,
+				"attributes"=>array(
+					array("supportedExtension")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
