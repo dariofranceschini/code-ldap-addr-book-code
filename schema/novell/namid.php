248dba6e568342fc90f320d2e2d288fcafaeb907
James Turner <james@turnersoft.co.uk>
2017-06-05 08:14:25 +0100

    reorder/reformat NAMID schema attributes

--- schema/novell/namid.php
+++ schema/novell/namid.php
@@ -6,8 +6,8 @@ class novell_namid_schema extends ldap_schema
 	function __construct(&$ldap_server)
 	{
 		$this->attribute_schema = array(
-			array("name"=>"uamPosixUidNumberReuse",		"data_type"=>"yes_no",	"display_name"=>gettext("Reuse UID Numbers")),
-			array("name"=>"uamPosixGidNumberReuse",		"data_type"=>"yes_no",	"display_name"=>gettext("Reuse GID Numbers")),
+			array("name"=>"uamPosixGidNumberReuse",		"data_type"=>"yes_no",		"display_name"=>gettext("Reuse GID Numbers")),
+			array("name"=>"uamPosixUidNumberReuse",		"data_type"=>"yes_no",		"display_name"=>gettext("Reuse UID Numbers")),
 			);
 
 		parent::__construct($ldap_server);
