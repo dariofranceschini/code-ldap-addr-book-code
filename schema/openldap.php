59dcbfcaaba865bf1d2525e4b96dc92b0f1e37ec
James Turner <james@turnersoft.co.uk>
2017-06-20 23:31:53 +0100

    define OpenLDAPdisplayableObject auxiliary class

--- schema/openldap.php
+++ schema/openldap.php
@@ -7,12 +7,11 @@ class openldap_schema extends ldap_schema
 	{
 		// Object classes
 		$this->object_schema = array(
+			array("name"=>"OpenLDAPdisplayableObject",		"icon"=>"generic24.png",	"class_type"=>"auxiliary","display_name"=>gettext("OpenLDAP Displayable Object")),
 			array("name"=>"OpenLDAPorg",				"icon"=>"org.png",		"is_folder"=>true,"display_name"=>gettext("OpenLDAP Organization"),"parent_class"=>"organization"),
 			array("name"=>"OpenLDAPou",				"icon"=>"folder.png",		"is_folder"=>true,"display_name"=>gettext("OpenLDAP Organizational Unit"),"parent_class"=>"organizationalUnit"),
 			array("name"=>"OpenLDAPperson",				"icon"=>"user24.png",		"is_folder"=>false,"display_name"=>gettext("OpenLDAP Person"),"parent_class"=>"pilotPerson,inetOrgPerson")
 			);
-
-		// auxiliary class 'OpenLDAPdisplayableObject' is also defined in this schema
 
 		parent::__construct($ldap_server);
 	}
