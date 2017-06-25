1c1b7b96bb5cf476744c72929677d178346ad063
James Turner <james@turnersoft.co.uk>
2017-06-25 10:10:42 +0100

    add nestedGroupAux object class definition and display layout

--- schema/novell/nestgrp.php
+++ schema/novell/nestgrp.php
@@ -11,6 +11,35 @@ class novell_nestgrp_schema extends ldap_schema
 			array("name"=>"nestedConfig",			"data_type"=>"nested_config",	"display_name"=>gettext("Nested Group Enforcement"))
 			);
 
+		// Object classes
+		$this->object_schema = array(
+			array("name"=>"nestedGroupAux",			"icon"=>"generic24.png",	"class_type"=>"auxiliary")
+			);
+
+		// Display layouts
+		$ldap_server->add_display_layout("nestedGroupAux",array(
+			array("section_name"=>gettext("Nested Group Enforcement"),
+				"attributes"=>array(
+					array("nestedConfig",		gettext("Enforcement Level"),		"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Groups Which Are Members (Static Only)"),"new_row"=>true,
+				"attributes"=>array(
+					array("groupMember")
+					)
+				),
+			array("section_name"=>gettext("Groups Which This is a Member Of (Static Only)"),"new_row"=>true,
+				"attributes"=>array(
+					array("groupMembership")
+					)
+				),
+			array("section_name"=>gettext("Objects Excluded from Nested Membership"),"new_row"=>true,
+				"attributes"=>array(
+					array("excludedMember")
+					)
+				)
+			));
+
 		parent::__construct($ldap_server);
 	}
 }
