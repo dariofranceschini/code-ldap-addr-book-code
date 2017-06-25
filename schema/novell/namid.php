e86deeec22d95310090e866f79f54956ffbf74e5
James Turner <james@turnersoft.co.uk>
2017-06-25 10:29:28 +0100

    fix spelling error

--- schema/novell/namid.php
+++ schema/novell/namid.php
@@ -13,7 +13,7 @@ class novell_namid_schema extends ldap_schema
 			array("name"=>"uamPosixGidNumberStart",		"data_type"=>"text",		"display_name"=>gettext("Starting GID Number")),
 
 			array("name"=>"uamPosixUidNumberDeletedMap",	"data_type"=>"download",	"display_name"=>gettext("Deleted UID Map")),
-			array("name"=>"uamPosixUidNumberEnd",		"data_type"=>"text",		"display_name"=>gettext("End UID Uumber")),
+			array("name"=>"uamPosixUidNumberEnd",		"data_type"=>"text",		"display_name"=>gettext("End UID Number")),
 			array("name"=>"uamPosixUidNumberLastAssigned",	"data_type"=>"text",		"display_name"=>gettext("Last Assigned UID")),
 			array("name"=>"uamPosixUidNumberReuse",		"data_type"=>"yes_no",		"display_name"=>gettext("Reuse UID Numbers")),
 			array("name"=>"uamPosixUidNumberStart",		"data_type"=>"text",		"display_name"=>gettext("Starting UID Number"))
