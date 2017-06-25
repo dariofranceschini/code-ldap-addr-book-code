09bc8353d490f65a2527b3c2d5efff9bb1844e32
James Turner <james@turnersoft.co.uk>
2017-06-25 12:11:01 +0100

    order UAM schema definitions alphabetically

--- schema/novell/uam.php
+++ schema/novell/uam.php
@@ -6,16 +6,16 @@ class novell_uam_schema extends ldap_schema
 	function __construct(&$ldap_server)
 	{
 		$this->attribute_schema = array(
+			array("name"=>"memberUid",			"data_type"=>"text_list",	"display_name"=>gettext("Member User ID")),
 			array("name"=>"uamPosixWorkstationContexts",	"data_type"=>"dn_list",		"display_name"=>gettext("POSIX Workstation Contexts")),
-			array("name"=>"uamPosixWorkstationList",	"data_type"=>"dn_list",		"display_name"=>gettext("POSIX Workstation List")),
-			array("name"=>"memberUid",			"data_type"=>"text_list",	"display_name"=>gettext("Member User ID"))
+			array("name"=>"uamPosixWorkstationList",	"data_type"=>"dn_list",		"display_name"=>gettext("POSIX Workstation List"))
 			);
 
 		// Object classes
 		$this->object_schema = array(
 			array("name"=>"posixGroup",			"icon"=>"group24.png",			"is_folder"=>false,"display_name"=>gettext("POSIX Group")),
-			array("name"=>"uamPosixWorkstation",		"icon"=>"novell/unix-workstation.png",	"is_folder"=>false),
 			array("name"=>"uamPosixConfig",			"icon"=>"novell/unix-config.png",	"is_folder"=>false),
+			array("name"=>"uamPosixWorkstation",		"icon"=>"novell/unix-workstation.png",	"is_folder"=>false)
 			);
 
 		// Display layouts
