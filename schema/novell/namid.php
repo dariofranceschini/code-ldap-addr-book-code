debb8009d1b3370c1c0b6c870bbdc1b9818d4ff1
James Turner <james@turnersoft.co.uk>
2017-06-25 10:56:29 +0100

    added NAMID auxiliary object class definitions and display layouts

--- schema/novell/namid.php
+++ schema/novell/namid.php
@@ -1,5 +1,5 @@
 <?php
-/** Novell Access Manager (NAM) ID schema (partial) */
+/** Novell Access Manager (NAM) ID schema */
 
 class novell_namid_schema extends ldap_schema
 {
@@ -18,6 +18,39 @@ class novell_namid_schema extends ldap_schema
 			array("name"=>"uamPosixUidNumberReuse",		"data_type"=>"yes_no",		"display_name"=>gettext("Reuse UID Numbers")),
 			array("name"=>"uamPosixUidNumberStart",		"data_type"=>"text",		"display_name"=>gettext("Starting UID Number"))
 			);
+
+		// Object classes
+
+		$this->object_schema = array(
+			array("name"=>"uamPosixGidNumberInfo",		"icon"=>"generic24.png",	"class_type"=>"auxiliary","display_name"=>gettext("Group ID Mapping Information")),
+			array("name"=>"uamPosixUidNumberInfo",		"icon"=>"generic24.png",	"class_type"=>"auxiliary","display_name"=>gettext("User ID Mapping Information"))
+			);
+
+		// Auxiliary class display layouts
+
+		$ldap_server->add_display_layout("uamPosixGidNumberInfo",array(
+			array("section_name"=>gettext("Group ID Mapping"),
+				"attributes"=>array(
+					array("uamPosixGidNumberStart",		gettext("Starting GID"),			"generic24.png"),
+					array("uamPosixGidNumberEnd",		gettext("End GID"),				"generic24.png"),
+					array("uamPosixGidNumberLastAssigned",	gettext("Last Assigned GID"),			"generic24.png"),
+					array("uamPosixGidNumberReuse",		gettext("Reuse GID Numbers"),			"generic24.png"),
+					array("uamPosixGidNumberDeletedMap",	gettext("Deleted GID Map"),			"generic24.png")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("uamPosixUidNumberInfo",array(
+			array("section_name"=>gettext("User ID Mapping"),
+				"attributes"=>array(
+					array("uamPosixUidNumberStart",		gettext("Starting UID"),			"generic24.png"),
+					array("uamPosixUidNumberEnd",		gettext("End UID"),				"generic24.png"),
+					array("uamPosixUidNumberLastAssigned",	gettext("Last Assigned UID"),			"generic24.png"),
+					array("uamPosixUidNumberReuse",		gettext("Reuse UID Numbers"),			"generic24.png"),
+					array("uamPosixUidNumberDeletedMap",	gettext("Deleted UID Map"),			"generic24.png")
+					)
+				)
+			));
 
 		parent::__construct($ldap_server);
 	}
