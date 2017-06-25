8632a22970ac82811e83c08561c6c92382a18c05
James Turner <james@turnersoft.co.uk>
2017-06-25 09:49:11 +0100

    sort DNIP schema definitions

--- schema/novell/dnip.php
+++ schema/novell/dnip.php
@@ -17,27 +17,27 @@ class novell_dnip_schema extends ldap_schema
 	function __construct(&$ldap_server)
 	{
 		$this->attribute_schema = array(
-			array("name"=>"dnipGroupReference",		"data_type"=>"dn_list",		"display_name"=>gettext("DNS/DHCP Group Object Name")),
-			array("name"=>"dnipDNSZones",			"data_type"=>"dn_list",		"display_name"=>gettext("DNS Zone Object Names")),
-			array("name"=>"dnipSubnetAttr",			"data_type"=>"dn_list",		"display_name"=>gettext("DHCP Subnet Attribute Object Names")),
-			array("name"=>"dnipComment",			"data_type"=>"text_area",	"display_name"=>gettext("Comments")),
 			array("name"=>"dnipAliasedObjectName",		"data_type"=>"dn_list",		"display_name"=>gettext("Associated NDS Object")),
-			array("name"=>"dnipSubnetPoolReference",	"data_type"=>"dn_list",		"display_name"=>gettext("Subnet Pool Reference")),
+			array("name"=>"dnipComment",			"data_type"=>"text_area",	"display_name"=>gettext("Comments")),
+			array("name"=>"dnipDNSZones",			"data_type"=>"dn_list",		"display_name"=>gettext("DNS Zone Object Names")),
+			array("name"=>"dnipGroupReference",		"data_type"=>"dn_list",		"display_name"=>gettext("DNS/DHCP Group Object Name")),
 			array("name"=>"dnipLocatorPtr",			"data_type"=>"dn",		"display_name"=>gettext("DNS/DHCP Locator DN")),
 			array("name"=>"dnipSecondaryZone",		"data_type"=>"yes_no",		"display_name"=>gettext("DNS Secondary Zone")),
+			array("name"=>"dnipSubnetAttr",			"data_type"=>"dn_list",		"display_name"=>gettext("DHCP Subnet Attribute Object Names")),
+			array("name"=>"dnipSubnetPoolReference",	"data_type"=>"dn_list",		"display_name"=>gettext("Subnet Pool Reference"))
 			);
 
 		// Object classes
 		$this->object_schema = array(
-			array("name"=>"dNIPLocator",			"icon"=>"dhcp/dhcp-locator.png",		"is_folder"=>false,"display_name"=>gettext("Locator")),
-			array("name"=>"dNIPDNSZone",			"icon"=>"novell/dnip-dns-zone.png",		"is_folder"=>true,"display_name"=>gettext("DNS Zone")),
-			array("name"=>"dNIPDNSRRset",			"icon"=>"document-series.png",			"is_folder"=>false,"display_name"=>gettext("DNS RRset")),
-			array("name"=>"dNIPSubnet",			"icon"=>"dhcp/dhcp-subnet.png",			"is_folder"=>true,"display_name"=>gettext("Subnet")),
-			array("name"=>"dNIPSubnetPool",			"icon"=>"dhcp/dhcp-shared-network.png",		"is_folder"=>false,"display_name"=>gettext("Subnet Pool")),
-			array("name"=>"dNIPSubnetAddressRange",		"icon"=>"novell/dnip-subnet-address-range.png",	"is_folder"=>false,"display_name"=>gettext("Subnet Address Range")),
-			array("name"=>"dNIPIPAddressConfiguration",	"icon"=>"dhcp/dhcp-host.png",			"is_folder"=>false,"display_name"=>gettext("IP Address Configuration")),
-			array("name"=>"dNIPDNSKey",			"icon"=>"dhcp/dhcp-dns-key.png",		"is_folder"=>false,"display_name"=>gettext("DNIP:DNS Key")),
 			array("name"=>"dNIPDHCPServer",			"icon"=>"dhcp/dhcp-server.png",			"is_folder"=>false,"display_name"=>gettext("DHCP Server (NetWare)")),
+			array("name"=>"dNIPDNSKey",			"icon"=>"dhcp/dhcp-dns-key.png",		"is_folder"=>false,"display_name"=>gettext("DNIP:DNS Key")),
+			array("name"=>"dNIPDNSRRset",			"icon"=>"document-series.png",			"is_folder"=>false,"display_name"=>gettext("DNS RRset")),
+			array("name"=>"dNIPDNSZone",			"icon"=>"novell/dnip-dns-zone.png",		"is_folder"=>true,"display_name"=>gettext("DNS Zone")),
+			array("name"=>"dNIPIPAddressConfiguration",	"icon"=>"dhcp/dhcp-host.png",			"is_folder"=>false,"display_name"=>gettext("IP Address Configuration")),
+			array("name"=>"dNIPLocator",			"icon"=>"dhcp/dhcp-locator.png",		"is_folder"=>false,"display_name"=>gettext("Locator")),
+			array("name"=>"dNIPSubnet",			"icon"=>"dhcp/dhcp-subnet.png",			"is_folder"=>true,"display_name"=>gettext("Subnet")),
+			array("name"=>"dNIPSubnetAddressRange",		"icon"=>"novell/dnip-subnet-address-range.png",	"is_folder"=>false,"display_name"=>gettext("Subnet Address Range")),
+			array("name"=>"dNIPSubnetPool",			"icon"=>"dhcp/dhcp-shared-network.png",		"is_folder"=>false,"display_name"=>gettext("Subnet Pool"))
 			);
 
 		// Display layouts
