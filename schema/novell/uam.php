8f08ffb13625c4e21d47d901e355b98a96989227
James Turner <james@turnersoft.co.uk>
2017-06-25 13:00:13 +0100

    added UAM auxiliary object class definitions and display layouts

--- schema/novell/uam.php
+++ schema/novell/uam.php
@@ -13,8 +13,12 @@ class novell_uam_schema extends ldap_schema
 
 		// Object classes
 		$this->object_schema = array(
+			array("name"=>"posixAccount",			"icon"=>"posix-account.png",		"class_type"=>"auxiliary","display_name"=>gettext("POSIX Account"),"required_attribs"=>"cn,uniqueID,uidNumber,gidNumber,homeDirectory"),
 			array("name"=>"posixGroup",			"icon"=>"group24.png",			"class_type"=>"auxiliary","display_name"=>gettext("POSIX Group"),"required_attribs"=>"cn,gidNumber"),
+			array("name"=>"shadowAccount",			"icon"=>"shadow-account.png",		"class_type"=>"auxiliary","required_attribs"=>"uniqueID"),
 			array("name"=>"uamPosixConfig",			"icon"=>"novell/unix-config.png",	"is_folder"=>false),
+			array("name"=>"uamPosixGroup",			"icon"=>"password.png",			"class_type"=>"auxiliary","display_name"=>gettext("UAM Workstation List")),
+			array("name"=>"uamPosixUser",			"icon"=>"password.png",			"class_type"=>"auxiliary","display_name"=>gettext("UAM Password Salt")),
 			array("name"=>"uamPosixWorkstation",		"icon"=>"novell/unix-workstation.png",	"is_folder"=>false)
 			);
 
@@ -37,6 +41,21 @@ class novell_uam_schema extends ldap_schema
 
 		// Auxiliary class layouts
 
+		$ldap_server->add_display_layout("posixAccount",array(
+			array("section_name"=>gettext("POSIX Account Settings"),
+				"attributes"=>array(
+					array("cn",				gettext("Object Name"),			"user24.png"),
+					array("uniqueID",			gettext("Unique ID"),			"generic24.png"),
+					array("uidNumber",			gettext("UID Number"),			"id.png"),
+					array("gidNumber",			gettext("GID Number"),			"id.png"),
+					array("homeDirectory",			gettext("Home Directory"),		"folder.png"),
+					array("loginShell",			gettext("Login Shell"),			"shell24.png"),
+					array("gecos",				gettext("GECOS Field"),			"description.png"),
+					array("description",			gettext("Description"),			"generic24.png"),
+					)
+				)
+			));
+
 		$ldap_server->add_display_layout("posixGroup",array(
 			array("section_name"=>gettext("POSIX Group Information"),
 				"attributes"=>array(
@@ -50,6 +69,38 @@ class novell_uam_schema extends ldap_schema
 					array("memberUid")
 					)
 				),
+			));
+
+		$ldap_server->add_display_layout("shadowAccount",array(
+			array("section_name"=>gettext("Shadow Password Settings"),
+				"attributes"=>array(
+					array("uniqueID",			gettext("Unique ID"),			"id.png"),
+					array("shadowLastChange",		gettext("Date of Last Password Change"),"generic24.png"),
+					array("shadowMin",			gettext("Minimum Password Age"),	"generic24.png"),
+					array("shadowMax",			gettext("Maximum Password Age"),	"generic24.png"),
+					array("shadowWarning",			gettext("Warning Period Before Expiration"),"generic24.png"),
+					array("shadowInactive",			gettext("Time Until Account Inactive"),	"generic24.png"),
+					array("shadowExpire",			gettext("Expiry Date"),			"generic24.png"),
+					// array("shadowFlag",			????,					"generic24.png"),	// attribute reserved for future use
+					array("description",			gettext("Description"),			"generic24.png")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("uamPosixGroup",array(
+			array("section_name"=>gettext("POSIX Workstation List"),
+				"attributes"=>array(
+					array("uamPosixWorkstationList")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("uamPosixUser",array(
+			array("section_name"=>gettext("Password Salt"),
+				"attributes"=>array(
+					array("uamPosixSalt")
+					)
+				)
 			));
 
 		parent::__construct($ldap_server);
