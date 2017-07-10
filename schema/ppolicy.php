7fb7bf34ae9fcae91597b4e445bf5843e998fa42
James Turner <james@turnersoft.co.uk>
2017-07-10 09:14:57 +0100

    definitions and display layouts for pwdPolicy and pwdPolicyChecker object classes

--- schema/ppolicy.php
+++ schema/ppolicy.php
@@ -67,10 +67,64 @@ class ppolicy_schema extends ldap_schema
 			array("name"=>"pwdStartTime",			"data_type"=>"date_time",	"display_name"=>gettext("Start of Password Validity Period"))
 			);
 
-		// Auxiliary object class 'pwdPolicyChecker' is also defined in this schema
-		// Auxiliary object class 'pwdPolicy' is also defined in this schema
+		// Object classes
+
+                $this->object_schema = array(
+			array("name"=>"pwdPolicy",			"icon"=>"password-policy.png",		"class_type"=>"auxiliary","display_name"=>gettext("Password Policy"),"required_attribs"=>"pwdAttribute"),
+			array("name"=>"pwdPolicyChecker",		"icon"=>"password-checker.png",		"class_type"=>"auxiliary","display_name"=>gettext("Password Policy Checker Module")),
+			);
+
+		// Auxiliary class display layouts
+
+		$ldap_server->add_display_layout("pwdPolicy",array(
+			array("section_name"=>gettext("Password Expiry Policy"),"new_row"=>true,
+				"attributes"=>array(
+					array("pwdMaxAge",			gettext("Maximum Password Age (s)"),						"generic24.png"),
+					array("pwdExpireWarning",		gettext("Length of Warning Period Before Password Expiry (s)"),			"generic24.png"),
+					array("pwdGraceAuthNLimit",		gettext("Number of Grace Logins Allowed After Expiry"),				"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Password Changing Policy"),"new_row"=>true,
+				"attributes"=>array(
+					array("pwdMinAge",			gettext("Minimum Password Age (s)"),						"generic24.png"),
+					array("pwdSafeModify",			gettext("Users Must Send Old and New Passwords When Changing"),			"generic24.png"),
+					array("pwdMustChange",			gettext("Users Must Change Password at Next Logon After Reset"),		"generic24.png"),
+					array("pwdAllowUserChange",		gettext("Allow Users to Change their Password"),				"generic24.png"),
+					array("pwdCheckQuality",		gettext("Check Quality of New Passwords"),					"generic24.png"),
+					array("pwdMinLength",			gettext("Minimum Accepted Password Length"),					"generic24.png"),
+					array("pwdInHistory",			gettext("Number of Passwords to Store in History"),				"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Consecutive Login Failures Policy"),"new_row"=>true,
+				"attributes"=>array(
+					array("pwdLockout",			gettext("Lock Accounts after Consecutive Login Failures"),			"generic24.png"),
+					array("pwdMaxFailure",			gettext("Number of Consecutive Login Failures before Policy Applies"),		"generic24.png"),
+					array("pwdLockoutDuration",		gettext("Account Lock Time after Consecutive Login Failures (s)"),		"generic24.png"),
+					array("pwdFailureCountInterval",	gettext("Reset Time for Failed Logins Counter (s)"),				"generic24.png")
+					)
+				),
+			array("section_name"=>gettext("Password Storage"),"new_row"=>true,
+				"attributes"=>array(
+					array("pwdAttribute",			gettext("Password Attribute"),							"generic24.png")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("pwdPolicyChecker",array(
+			array("section_name"=>gettext("Password Policy Enforcement"),
+				"attributes"=>array(
+					array("pwdCheckModule",			gettext("Password Check Module"),						"password-checker.png")
+					)
+				)
+			));
 
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_pwdPolicy(&$ldap_server,&$entry)
+	{
+		$this->add_attrib_value($ldap_server,$entry,"pwdAttribute","userPassword");
+		$this->add_attrib_value($ldap_server,$entry,"pwdMustChange","FALSE");
 	}
 }
 ?>
