b911e3bda20c2f963af726ee61d7d8b8e5b7ade8
James Turner <james@turnersoft.co.uk>
2017-07-24 09:07:52 +0100

    auxiliary class definitions and layouts for SCHAC schema

--- schema/schac.php
+++ schema/schac.php
@@ -32,21 +32,141 @@ class schac_schema extends ldap_schema
 			array("name"=>"schacYearOfBirth",		"data_type"=>"text",		"display_name"=>gettext("Year of Birth"))
 			);
 
-		// No structural object classes for this schema (auxillary classes only)
+                // Object classes
+                $this->object_schema = array(
+			array("name"=>"schacContactLocation",		"icon"=>"locality.png",		"class_type"=>"auxiliary","display_name"=>gettext("Contact Location"),"can_create"=>true),
+			array("name"=>"schacEmployeeInfo",		"icon"=>"org-role.png",		"class_type"=>"auxiliary","display_name"=>gettext("Position Within Institution"),"can_create"=>true),
+			array("name"=>"schacEntryConfidentiality",	"icon"=>"generic24.png",	"class_type"=>"auxiliary","display_name"=>gettext("Private Attributes List"),"can_create"=>true),
+			array("name"=>"schacEntryMetadata",		"icon"=>"date-time.png",	"class_type"=>"auxiliary","display_name"=>gettext("Authorization Expiry Date/Time"),"can_create"=>true),
+			array("name"=>"schacExperimentalOC",		"icon"=>"generic24.png",	"class_type"=>"auxiliary","display_name"=>gettext("SCHAC Experimental Attributes")),
+			array("name"=>"schacGroupMembership",		"icon"=>"group24.png",		"class_type"=>"auxiliary","display_name"=>gettext("Project Group Membership"),"can_create"=>true),
+			array("name"=>"schacLinkageIdentifiers",	"icon"=>"user-alias24.png",	"class_type"=>"auxiliary","display_name"=>gettext("External Identifiers"),"can_create"=>true),
+			array("name"=>"schacPersonalCharacteristics",	"icon"=>"generic24.png",	"class_type"=>"auxiliary","display_name"=>gettext("Personal Characteristics"),"can_create"=>true),
+			array("name"=>"schacUserEntitlements",		"icon"=>"generic24.png",	"class_type"=>"auxiliary","display_name"=>gettext("Authorization for Services"),"can_create"=>true)
+			);
 
-		// The auxiliary classes defined in this schema are:
-		//
-		//	schacContactLocation
-		//	schacEmployeeInfo
-		//	schacEntryConfidentiality
-		//	schacEntryMetadata
-		//	schacExperimentalOC
-		//	schacGroupMembership
-		//	schacLinkageIdentifiers
-		//	schacPersonalCharacteristics
-		//	schacUserEntitlements
+                // Auxiliary class display layouts
+
+		$ldap_server->add_display_layout("schacPersonalCharacteristics",array(
+			array("section_name"=>gettext("Personal Characteristics"),
+				"attributes"=>array(
+					array("schacPersonalTitle",		gettext("Personal Title"),			"generic24.png"),
+					array("schacSn1",			gettext("First Surname"),			"generic24.png"),
+					array("schacSn2",			gettext("Second Surname"),			"generic24.png"),
+					array("schacGender",			gettext("Gender"),				"generic24.png"),
+					array("schacDateOfBirth",		gettext("Date of Birth"),			"date.png"),
+					array("schacPlaceOfBirth",		gettext("Place of Birth"),			"locality.png"),
+					array("schacCountryOfCitizenship",	gettext("Country of Citizenship"),		"country.png"),
+					array("schacMotherTongue",		gettext("Preferred Language"),			"chat.png"),
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("schacContactLocation",array(
+			array("section_name"=>gettext("Contact Location"),
+				"attributes"=>array(
+					array("schacHomeOrganization",		gettext("Home Organization Domain Name"),	"domain24.png"),
+					array("schacHomeOrganizationType",	gettext("Home Organization Type"),		"generic24.png"),
+					array("schacCountryOfResidence",	gettext("Country of Residence"),		"country.png"),
+					array("schacUserPresenceID",		gettext("Presence Service URL"),		"labeled-uri.png")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("schacEmployeeInfo",array(
+			array("section_name"=>gettext("Position Within Institution"),
+				"attributes"=>array(
+					array("schacPersonalPosition")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("schacLinkageIdentifiers",array(
+			array("section_name"=>gettext("External Identifiers"),
+				"attributes"=>array(
+					array("schacPersonalUniqueCode",	gettext("Personal Unique Code"),		"id.png"),
+					array("schacPersonalUniqueID",		gettext("Personal Unique ID"),			"id.png")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("schacEntryMetadata",array(
+			array("section_name"=>gettext("Authorization Expiry Date/Time"),
+				"attributes"=>array(
+					array("schacExpiryDate")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("schacEntryConfidentiality",array(
+			array("section_name"=>gettext("Private Attributes"),
+				"attributes"=>array(
+					array("schacUserPrivateAttribute")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("schacUserEntitlements",array(
+			array("section_name"=>gettext("Authorization for Services"),
+				"attributes"=>array(
+					array("schacUserStatus")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("schacGroupMembership",array(
+			array("section_name"=>gettext("Project Group Membership"),
+				"attributes"=>array(
+					array("schacProjectMembership",		gettext("Project Name"),			"generic24.png"),
+					array("schacProjectSpecificRole",	gettext("Roles Within Project"),		"org-role.png")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("schacExperimentalOC",array(
+			array("section_name"=>gettext("SCHAC Experimental Attributes"),
+				"attributes"=>array(
+					array("schacYearOfBirth",		gettext("Year of Birth"),			"date.png")
+					)
+				)
+			));
 
 		parent::__construct($ldap_server);
+	}
+
+	function populate_for_create_schacGroupMembership(&$ldap_server,&$entry)
+	{
+		$this->add_attrib_value($ldap_server,$entry,"schacProjectSpecificRole",
+			"urn:mace:terena.org:schac:projectSpecificRole:<project-name>:<iNSS>");
+	}
+
+	// note: country code can be replaced with <institution-code>.<country-code>
+	// where <institution-code> is typically 3 letters
+
+	function populate_for_create_schacUserEntitlements(&$ldap_server,&$entry)
+	{
+		$this->add_attrib_value($ldap_server,$entry,"schacUserStatus",
+			"urn:mace:terena.org:schac:userStatus:<country-code>:<domain>:<iNSS>");
+	}
+
+	function populate_for_create_schacLinkageIdentifiers(&$ldap_server,&$entry)
+	{
+		$this->add_attrib_value($ldap_server,$entry,"schacPersonalUniqueCode",
+			"urn:mace:terena.org:schac:personalUniqueCode:<country-code>:<iNSS>");
+		$this->add_attrib_value($ldap_server,$entry,"schacPersonalUniqueID",
+			"urn:mace:terena.org:schac:personalUniqueID:<country-code>:<idType>:<idValue>");
+	}
+
+	function populate_for_create_schacEmployeeInfo(&$ldap_server,&$entry)
+	{
+		$this->add_attrib_value($ldap_server,$entry,"schacPersonalPosition",
+			"urn:mace:terena.org:schac:personalPosition:<country-code>:<domain>:<iNSS>");
+	}
+
+	function populate_for_create_schacContactLocation(&$ldap_server,&$entry)
+	{
+		$this->add_attrib_value($ldap_server,$entry,"schacHomeOrganizationType",
+			"urn:mace:terena.org:schac:homeOrganizationType:<country-code>:<string>");
 	}
 }
 ?>
