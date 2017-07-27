0c5339b423f9c66c6447b88e2e356cf89ce687fe
James Turner <james@turnersoft.co.uk>
2017-07-27 09:03:55 +0100

    fix get_user_setting() called before $ldap_server->log_on()

--- index.php
+++ index.php
@@ -24,83 +24,83 @@ if(empty($_GET["vcard"]))
 
 if(prereq_components_ok())
 {
-	// TODO: sanitise base DN from URL:
-	//	stop "nasties" being passed through to the LDAP server
-
-	if(isset($_GET["dn"]) && strlen($_GET["dn"])<=MAX_DN_LENGTH
-			&& ($ldap_server->compare_dn_to_base($_GET["dn"],$ldap_base_dn)
-			|| get_user_setting("allow_system_admin")))
-		$dn = $_GET["dn"];
-	else
-		$dn = $ldap_base_dn;
-
-	// Default filter expression to use if none specified in config
-	// file: return only people in search results
-	if(empty($search_ldap_filter))
-		$search_ldap_filter
-			= "(&(objectClass=person)___search_criteria___)";
-
-	if(!empty($_GET["filter"]))
+	if($ldap_server->log_on())
 	{
-		$filter = ldap_escape($_GET["filter"],null,LDAP_ESCAPE_FILTER);
+		// TODO: sanitise base DN from URL:
+		//	stop "nasties" being passed through to the LDAP server
 
-		if(!isset($search_method)) $search_method = 1;
+		if(isset($_GET["dn"]) && strlen($_GET["dn"])<=MAX_DN_LENGTH
+				&& ($ldap_server->compare_dn_to_base($_GET["dn"],$ldap_base_dn)
+				|| get_user_setting("allow_system_admin")))
+			$dn = $_GET["dn"];
+		else
+			$dn = $ldap_base_dn;
 
-		switch($search_method)
+		// Default filter expression to use if none specified in config
+		// file: return only people in search results
+		if(empty($search_ldap_filter))
+			$search_ldap_filter
+				= "(&(objectClass=person)___search_criteria___)";
+
+		$search_resource = false;
+
+		if(!empty($_GET["filter"]))
 		{
-			case 2: // match start of string
-				$attribute_search_template = "(___search_attrib___=___search_value___*)";
-				break;
-			case 1:	// match anywhere in string
-			default:
-				$attribute_search_template = "(___search_attrib___=*___search_value___*)";
-				break;
+			$filter = ldap_escape($_GET["filter"],null,LDAP_ESCAPE_FILTER);
+
+			if(!isset($search_method)) $search_method = 1;
+
+			switch($search_method)
+			{
+				case 2: // match start of string
+					$attribute_search_template = "(___search_attrib___=___search_value___*)";
+					break;
+				case 1:	// match anywhere in string
+				default:
+					$attribute_search_template = "(___search_attrib___=*___search_value___*)";
+					break;
+			}
+
+			$search_criteria = "";
+			foreach($search_ldap_attrib as $attrib)
+				$search_criteria .= str_replace("___search_value___",$filter,
+					str_replace("___search_attrib___",$attrib,
+					$attribute_search_template));
+
+			$filter = str_replace("___search_criteria___",
+				"(|" . $search_criteria . ")",$search_ldap_filter);
+
+			$search_type= "subtree";
 		}
-
-		$search_criteria = "";
-		foreach($search_ldap_attrib as $attrib)
-			$search_criteria .= str_replace("___search_value___",$filter,
-				str_replace("___search_attrib___",$attrib,
-				$attribute_search_template));
-
-		$filter = str_replace("___search_criteria___",
-			"(|" . $search_criteria . ")",$search_ldap_filter);
-
-		$search_type= "subtree";
-	}
-	else if(get_user_setting("display_all_records_by_default") && $dn == $ldap_base_dn)
-	{
-		$filter = str_replace("___search_criteria___",
-			"(objectClass=*)",$search_ldap_filter);
-		$search_type= "subtree";
-	}
-	else
-	{
-		if(empty($_GET["vcard"]))
+		else if(get_user_setting("display_all_records_by_default") && $dn == $ldap_base_dn)
 		{
-			// Default filter expression to use if none specified in config
-			// file: display objects of all classes when browsing the directory
-			$filter = empty($browse_ldap_filter)
-				?"objectClass=*":$browse_ldap_filter;
-			$search_type= "base";
+			$filter = str_replace("___search_criteria___",
+				"(objectClass=*)",$search_ldap_filter);
+			$search_type= "subtree";
 		}
 		else
 		{
-			// vCard export settings
-			$filter = empty($search_ldap_filter)
-				?"objectClass=person":$search_ldap_filter;
+			if(empty($_GET["vcard"]))
+			{
+				// Default filter expression to use if none specified in config
+				// file: display objects of all classes when browsing the directory
+				$filter = empty($browse_ldap_filter)
+					?"objectClass=*":$browse_ldap_filter;
+				$search_type= "base";
+			}
+			else
+			{
+				// vCard export settings
+				$filter = empty($search_ldap_filter)
+					?"objectClass=person":$search_ldap_filter;
 
-			$filter = str_replace("___search_criteria___",
-				"",$search_ldap_filter);
+				$filter = str_replace("___search_criteria___",
+					"",$search_ldap_filter);
 
-			$search_type = "subtree";
+				$search_type = "subtree";
+			}
 		}
-	}
 
-	$search_resource = false;
-
-	if($ldap_server->log_on())
-	{
 		if(empty($_GET["vcard"]))
 		{
 			show_ldap_path($dn);
@@ -148,75 +148,77 @@ if(prereq_components_ok())
 				else
 					echo "<p>" . gettext("You do not have permission to browse the directory") . "</p>\n";
 			}
-	}
-	else
-		show_ldap_bind_error();
 
-	if($ldap_server->server_type == "openldap" && strcasecmp($dn,"cn=config")==0)
-	{
-		$search_result_columns = array(
-			array("caption"=>gettext("OpenLDAP Server Configuration"),
-			"attrib"=>"sortableName","link_type"=>"object"));
-	}
+		if($ldap_server->server_type == "openldap" && strcasecmp($dn,"cn=config")==0)
+		{
+			$search_result_columns = array(
+				array("caption"=>gettext("OpenLDAP Server Configuration"),
+				"attrib"=>"sortableName","link_type"=>"object"));
+		}
 
-	// Display search resource info if successfully fetched
-	if(is_resource($search_resource))
-	{
-		if(!empty($_GET["sort"]))
-			$sort_type = $_GET["sort"];
-		else
-			$sort_type = $search_result_default_sort_order;
+		// Display search resource info if successfully fetched
+		if(is_resource($search_resource))
+		{
+			if(!empty($_GET["sort"]))
+				$sort_type = $_GET["sort"];
+			else
+				$sort_type = $search_result_default_sort_order;
 
-		// Only allow sorting on attributes which are actualy used in columns
-		$sort_order=$search_result_default_sort_order;
-		foreach($search_result_columns as $column)
-			if($column["attrib"] == $sort_type);
-				$sort_order = $sort_type;
+			// Only allow sorting on attributes which are actualy used in columns
+			$sort_order=$search_result_default_sort_order;
+			foreach($search_result_columns as $column)
+				if($column["attrib"] == $sort_type);
+					$sort_order = $sort_type;
 
-		$entry_list = new ldap_entry_list($ldap_server,$search_resource,
-			$search_result_columns,$sort_order);
+			$entry_list = new ldap_entry_list($ldap_server,$search_resource,
+				$search_result_columns,$sort_order);
 
-		if(!empty($_GET["filter"]))
-			$entry_list->contains_search_results=true;
+			if(!empty($_GET["filter"]))
+				$entry_list->contains_search_results=true;
+
+			if(empty($_GET["vcard"]))
+			{
+				// extra space between LDAP path and results if no search box
+				if(!get_user_setting("allow_search"))
+					echo "<br>";
+
+				$entry_list->show();
+			}
+			else
+				$entry_list->save_vcard($dn);
+		}
+
+		echo "\n";
 
 		if(empty($_GET["vcard"]))
 		{
-			// extra space between LDAP path and results if no search box
-			if(!get_user_setting("allow_search"))
-				echo "<br>";
+			$buttons = "";
 
-			$entry_list->show();
+			if(empty($_GET["filter"]) && get_user_setting("allow_folder_info")
+					&& get_user_setting("allow_browse") && get_user_setting("allow_view"))
+				$buttons .= "<a href=\"info.php?dn="
+					. urlencode($dn)
+						. "\"><button>" . gettext("Folder Details") . "</button></a>\n";
+
+			if(get_user_setting("allow_create"))
+				$buttons .= "<a href=\"create.php?dn="
+					. urlencode($dn)
+					. "\"><button>" . gettext("New Record") . "</button></a>\n";
+
+			if(get_user_setting("allow_export_bulk") && empty($_GET["filter"]))
+				$buttons .= "<a href=\"index.php?vcard=1&dn="
+					. urlencode($dn)
+					. "\"><button>" . gettext("Export Records") . "</button></a>\n";
+
+			if(!empty($buttons))
+				echo "<hr class=\"button_separator_line\">\n" . $buttons;
+
 		}
-		else
-			$entry_list->save_vcard($dn);
 	}
-
-	echo "\n";
+	else
+		show_ldap_bind_error();
 }
 
 if(empty($_GET["vcard"]))
-{
-	$buttons = "";
-
-	if(empty($_GET["filter"]) && get_user_setting("allow_folder_info")
-			&& get_user_setting("allow_browse") && get_user_setting("allow_view"))
-		$buttons .= "<a href=\"info.php?dn="
-			. urlencode($dn)
-				. "\"><button>" . gettext("Folder Details") . "</button></a>\n";
-
-	if(get_user_setting("allow_create"))
-		$buttons .= "<a href=\"create.php?dn="
-			. urlencode($dn)
-			. "\"><button>" . gettext("New Record") . "</button></a>\n";
-
-	if(get_user_setting("allow_export_bulk") && empty($_GET["filter"]))
-		$buttons .= "<a href=\"index.php?vcard=1&dn="
-			. urlencode($dn)
-			. "\"><button>" . gettext("Export Records") . "</button></a>\n";
-
-	if(!empty($buttons))
-		echo "<hr class=\"button_separator_line\">\n" . $buttons;
-
 	show_site_footer();
-}
 ?>
