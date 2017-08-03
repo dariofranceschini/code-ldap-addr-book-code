092060380ef767037f0d01cf950510b4199d4a90
James Turner <james@turnersoft.co.uk>
2017-08-03 09:35:04 +0100

    defined attributes for representing AD replication topology

--- schema/microsoft.php
+++ schema/microsoft.php
@@ -10,6 +10,7 @@ class microsoft_schema extends ldap_schema
 			array("name"=>"adminMultiSelectPropertyPages",	"data_type"=>"text_list",	"display_name"=>gettext("Admin Multi Select Property Pages")),
 			array("name"=>"adminPropertyPages",		"data_type"=>"text_list",	"display_name"=>gettext("Admin Property Pages")),
 			array("name"=>"attributeDisplayNames",		"data_type"=>"text_list",	"display_name"=>gettext("Attribute Display Names")),
+			array("name"=>"bridgeheadTransportList",	"data_type"=>"dn_list",		"display_name"=>gettext("Bridgehead Transports")),
 			array("name"=>"canUpgradeScript",		"data_type"=>"text_list",	"display_name"=>gettext("Package Upgrade Relationships")),
 			array("name"=>"categories",			"data_type"=>"text_list",	"display_name"=>gettext("Category GUIDs")),
 			array("name"=>"company",			"data_type"=>"text",		"display_name"=>gettext("Company")),
@@ -17,33 +18,50 @@ class microsoft_schema extends ldap_schema
 			array("name"=>"createWizardExt",		"data_type"=>"text_list",	"display_name"=>gettext("Object Creation Wizard Extensions")),
 			array("name"=>"department",			"data_type"=>"text",		"display_name"=>gettext("Department")),
 			array("name"=>"displayName",			"data_type"=>"text",		"display_name"=>gettext("Display/Preferred Name")),
+			array("name"=>"dMDLocation",			"data_type"=>"dn",		"display_name"=>gettext("Schema Partition DN")),
+			array("name"=>"enabledConnection",		"data_type"=>"yes_no",		"display_name"=>gettext("Connection Is Enabled")),
 			array("name"=>"extendedAttributeInfo",		"data_type"=>"ldap_schema",	"display_name"=>gettext("Extended Attribute Information")),
 			array("name"=>"extendedClassInfo",		"data_type"=>"ldap_schema",	"display_name"=>gettext("Extended Class Information")),
 			array("name"=>"extraColumns",			"data_type"=>"text_list",	"display_name"=>gettext("Extra Columns")),
+			array("name"=>"fromServer",			"data_type"=>"dn",		"display_name"=>gettext("Replication Source Server")),
 			array("name"=>"groupType",			"data_type"=>"ad_group_type",	"display_name"=>gettext("Group Type/Scope")),
+			array("name"=>"hasMasterNCs",			"data_type"=>"dn_list",		"display_name"=>gettext("Naming Contexts Served")),
 			array("name"=>"info",				"data_type"=>"text_area",	"display_name"=>gettext("General Information")),
+			array("name"=>"invocationId",			"data_type"=>"download",	"display_name"=>gettext("Invocation ID Data")),
 			array("name"=>"managedBy",			"data_type"=>"dn",		"display_name"=>gettext("Managed By")),
 			array("name"=>"memberOf",			"data_type"=>"dn_list",		"display_name"=>gettext("Member Of")),
 			array("name"=>"msDS-AllowedToDelegateTo",	"data_type"=>"text_list",	"display_name"=>gettext("Allowed to Delegate To")),
+			array("name"=>"msDS-HasDomainNCs",		"data_type"=>"dn_list",		"display_name"=>gettext("Domain Naming Contexts")),
+			array("name"=>"msDS-HasInstantiatedNCs",	"data_type"=>"text_list",	"display_name"=>gettext("Naming Context Replication Status")),
 			array("name"=>"msiFileList",			"data_type"=>"text_list",	"display_name"=>gettext("Package Deployment Source List")),
 			array("name"=>"msWMI-ChangeDate",		"data_type"=>"date_time",	"display_name"=>gettext("WMI Object Last Modification Date")),
 			array("name"=>"msWMI-CreationDate",		"data_type"=>"date_time",	"display_name"=>gettext("WMI Object Creation Date")),
+
+			// Bitfield, the interpretation of which varies according to the object class
+			// in which it is being used.
+			array("name"=>"options",			"data_type"=>"text",		"display_name"=>gettext("Options")),
+
 			array("name"=>"printColor",			"data_type"=>"yes_no",		"display_name"=>gettext("Color Printing Supported")),
 			array("name"=>"printDuplexSupported",		"data_type"=>"yes_no",		"display_name"=>gettext("Double-Sided Printing Supported")),
 			array("name"=>"printMediaReady",		"data_type"=>"text_list",	"display_name"=>gettext("Paper Available")),
 			array("name"=>"printMediaSupported",		"data_type"=>"text_list",	"display_name"=>gettext("Paper Supported")),
 			array("name"=>"printStaplingSupported",		"data_type"=>"yes_no",		"display_name"=>gettext("Stapling Supported")),
+			array("name"=>"queryPolicyObject",		"data_type"=>"dn",		"display_name"=>gettext("Query Policy Object DN")),
 			array("name"=>"sAMAccountName",			"data_type"=>"text",		"display_name"=>gettext("Pre-Windows 2000 Account Name")),
+			array("name"=>"schedule",			"data_type"=>"download",	"display_name"=>gettext("Replication Schedule")),
 
 			// Data type of "serverName" is changed to "dn" when used with the Active Directory
 			// Root DSE object (hardcoded workaround). The data type "text" (as defined below) is
 			// used for the "serverName" attribute of "printQueue" objects.
 			array("name"=>"serverName",			"data_type"=>"text",		"display_name"=>gettext("Server Name")),
 
+			array("name"=>"serverReference",		"data_type"=>"dn",		"display_name"=>gettext("Server's Computer Object DN")),
 			array("name"=>"servicePrincipalName",		"data_type"=>"text_list",	"display_name"=>gettext("Service Principal Names")),
 			array("name"=>"shellPropertyPages",		"data_type"=>"text_list",	"display_name"=>gettext("Shell Property Pages")),
 			// Non-standard implementation of streetAddress as a separate attribute, rather than an alias of street:
 			array("name"=>"streetAddress",			"data_type"=>"text_area",	"display_name"=>gettext("Street Address")),
+
+			array("name"=>"transportType",			"data_type"=>"dn",		"display_name"=>gettext("Replication Transport")),
 			array("name"=>"treatAsLeaf",			"data_type"=>"yes_no",		"display_name"=>gettext("Treat as Leaf Object")),
 			array("name"=>"url",				"data_type"=>"text",		"display_name"=>gettext("URL (e.g. web page)")),
 			array("name"=>"whenCreated",			"data_type"=>"date_time",	"display_name"=>gettext("Creation Date")),
