2acfe27cfc1df98a4d9f2e9b689a45d1affee82c
James Turner <james@turnersoft.co.uk>
2017-09-04 09:58:21 +0100

    more explanatory section title for TLS config settings

--- schema/openldap/config.php
+++ schema/openldap/config.php
@@ -55,7 +55,7 @@ class openldap_config_schema extends ldap_schema
 					array("olcToolThreads",		gettext("Number of Tool Threads"),		"generic24.png")
 					)
 				),
-			array("section_name"=>gettext("TLS Settings"),"new_row"=>true,
+			array("section_name"=>gettext("Transport Layer Security (TLS)"),"new_row"=>true,
 				"attributes"=>array(
 					array("olcTLSCACertificateFile",gettext("CA Certificate File"),			"generic24.png"),
 					array("olcTLSCertificateFile",	gettext("Server Certificate File"),		"generic24.png"),
