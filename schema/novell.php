dde12e76cb50cd51bb85faa39ea8cad2f92170a7
James Turner <james@turnersoft.co.uk>
2017-12-23 14:17:38 +0000

    move operational/schema definition attributes to eDirectory base schema

--- schema/novell.php
+++ schema/novell.php
@@ -32,6 +32,32 @@ class novell_schema extends ldap_schema
 			array("name"=>"hostServer",			"data_type"=>"dn",		"display_name"=>gettext("Host Server")),
 			array("name"=>"loginScript",			"data_type"=>"text_area",	"display_name"=>gettext("Login Script")),
 			array("name"=>"ndsCrossCertificatePair",	"data_type"=>"download",	"display_name"=>gettext("Cross Certificate Pair (NDS)")),
+
+			// Root DSE attributes
+			array("name"=>"altServer",                      "data_type"=>"text_list",       "display_name"=>gettext("Alternative Servers")),
+			array("name"=>"dSAName",                        "data_type"=>"dn",              "display_name"=>gettext("DSA Name")),
+			array("name"=>"namingContexts",                 "data_type"=>"dn_list",         "display_name"=>gettext("Naming Contexts")),
+			array("name"=>"subschemaSubentry",              "data_type"=>"dn",              "display_name"=>gettext("Subschema Subentry")),
+			array("name"=>"supportedControl",               "data_type"=>"oid_list",        "display_name"=>gettext("Supported Controls")),
+			array("name"=>"supportedExtension",             "data_type"=>"oid_list",        "display_name"=>gettext("Supported Extended Operations")),
+			array("name"=>"supportedFeatures",              "data_type"=>"oid_list",        "display_name"=>gettext("Supported Features")),
+			// See: draft-zeilenga-ldap-grouping-06
+			array("name"=>"supportedGroupingTypes",         "data_type"=>"oid_list",        "display_name"=>gettext("Supported Grouping Types")),
+			array("name"=>"supportedLDAPVersion",           "data_type"=>"ldap_version",    "display_name"=>gettext("Supported LDAP Versions")),
+			array("name"=>"supportedSASLMechanisms",        "data_type"=>"text_list",       "display_name"=>gettext("Supported SASL Mechanisms")),
+
+			// Schema definition attributes
+			array("name"=>"attributeTypes",                 "data_type"=>"ldap_schema",     "display_name"=>gettext("Attribute Types")),
+			array("name"=>"ldapSyntaxes",                   "data_type"=>"ldap_schema",     "display_name"=>gettext("LDAP Syntaxes")),
+			array("name"=>"matchingRules",                  "data_type"=>"ldap_schema",     "display_name"=>gettext("Matching Rules")),
+			array("name"=>"matchingRuleUse",                "data_type"=>"ldap_schema",     "display_name"=>gettext("Matching Rule Use")),
+			array("name"=>"objectClasses",                  "data_type"=>"ldap_schema",     "display_name"=>gettext("Object Classes")),
+
+			// Operational attributes
+			array("name"=>"creatorsName",                   "data_type"=>"dn",              "display_name"=>gettext("Created By")),
+			array("name"=>"createTimestamp",                "data_type"=>"date_time",       "display_name"=>gettext("Created")),
+			array("name"=>"modifiersName",                  "data_type"=>"dn",              "display_name"=>gettext("Last Modified By")),
+			array("name"=>"modifyTimestamp",                "data_type"=>"date_time",       "display_name"=>gettext("Last Modified"))
 			);
 
 		// Object classes
@@ -47,6 +73,7 @@ class novell_schema extends ldap_schema
 			array("name"=>"organizationalRole",		"icon"=>"org-role.png",			"is_folder"=>false,"display_name"=>gettext("Organizational Role"),"can_create"=>true),
 			array("name"=>"organizationalUnit",		"icon"=>"folder.png",			"is_folder"=>true,"rdn_attrib"=>"ou","display_name"=>gettext("Organizational Unit"),"can_create"=>true,"parent_class"=>"ndsLoginProperties,ndsContainerLoginProperties"),
 			array("name"=>"Person",				"icon"=>"contact24.png",		"is_folder"=>false,"display_name"=>gettext("Person"),"required_attribs"=>"sn","can_create"=>true,"parent_class"=>"ndsLoginProperties"),
+			array("name"=>"subschema",			"icon"=>"schema.png",			"is_folder"=>false,"display_name"=>gettext("Schema")),
 
 			// Novell proprietary classes
 			array("name"=>"aliasObject",			"icon"=>"alias.png",			"is_folder"=>false,"display_name"=>gettext("Alias"),"required_attribs"=>"aliasedObjectName"),
@@ -233,6 +260,24 @@ class novell_schema extends ldap_schema
 			array("section_name"=>gettext("Role Occupants"),"new_row"=>true,
 				"attributes"=>array(
 					array("roleOccupant")
+					)
+				)
+			));
+
+		$ldap_server->add_display_layout("subschema",array(
+			array("section_name"=>gettext("Object Class Definitions"),"new_row"=>true,"colspan"=>2,
+				"attributes"=>array(
+					array("objectClasses")
+					)
+				),
+			array("section_name"=>gettext("Attribute Type Definitions"),"new_row"=>true,"colspan"=>2,
+				"attributes"=>array(
+					array("attributeTypes")
+					)
+				),
+			array("section_name"=>gettext("LDAP Syntax Definitions"),"new_row"=>true,"colspan"=>2,
+				"attributes"=>array(
+					array("ldapSyntaxes")
 					)
 				)
 			));
